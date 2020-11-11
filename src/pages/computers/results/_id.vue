b7ba58447dc9985b9a2b165b267337d6ae4a386b
jact <jachavar@gmail.com>
2020-11-11 12:15:16 +0100

    Added Breadcrumbs component

--- src/pages/computers/results/_id.vue
+++ src/pages/computers/results/_id.vue
@@ -1,27 +1,24 @@
 <template>
   <q-page padding>
-    <q-breadcrumbs>
-      <q-breadcrumbs-el
-        v-for="(item, index) in breadcrumbs"
-        :key="index"
-        :icon="item.icon"
-        :to="item.href"
-        :label="item.text"
-      />
-    </q-breadcrumbs>
+    <Breadcrumbs :items="breadcrumbs" />
 
     {{ element }}
   </q-page>
 </template>
 
 <script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+
 export default {
+  components: {
+    Breadcrumbs
+  },
   data() {
     return {
       breadcrumbs: [
         {
           text: 'Dashboard',
-          href: '/',
+          to: 'home',
           icon: 'mdi-home'
         },
         {
@@ -30,18 +27,17 @@ export default {
         },
         {
           text: 'Ordenadores',
-          href: '/computer',
+          to: 'computers-dashboard'
         },
         {
           text: 'Resultados',
-          href: '/computer/results'
+          to: 'computers-list'
         },
         {
-          text: 'Id',
-          disabled: true,
-        },
+          text: 'Id'
+        }
       ],
-      element: {},
+      element: {}
     }
   },
   async mounted() {
@@ -49,12 +45,12 @@ export default {
       .get(`/api/v1/token/computers/${this.$route.params.id}/`)
       .then((response) => {
         console.log(response)
-        this.item = response.data
-        this.breadcrumbs[4].text = this.item.name
+        this.element = response.data
+        this.breadcrumbs[4].text = this.element.name
       })
       .catch((error) => {
-          this.$store.dispatch('ui/notifyError', error.response.data.detail)
+        this.$store.dispatch('ui/notifyError', error.response.data.detail)
       })
-  },
+  }
 }
 </script>
