27304d0066165b47cdd50542f137b93f7cbe75d0
jact <jachavar@gmail.com>
2020-11-11 12:15:41 +0100

    Improved component

--- src/components/MigasLink.vue
+++ src/components/MigasLink.vue
@@ -1,5 +1,15 @@
 <template>
-  <q-btn-dropdown color="primary" outline split no-caps push dense :to="link">
+  <q-btn-dropdown
+    color="primary"
+    outline
+    split
+    no-caps
+    push
+    dense
+    :loading="loading"
+    :to="link"
+    @show="getRelations"
+  >
     <template v-slot:label>
       <q-icon v-if="icon" left :name="icon" />
       {{ value }}
@@ -7,6 +17,38 @@
         {{ tooltip }}
       </q-tooltip>
     </template>
+
+    <q-list>
+      <q-item
+        v-for="(item, index) in relations"
+        :key="index"
+        v-close-popup
+        clickable
+      >
+        <q-item-section>
+          <q-chip outline color="primary">
+            <q-avatar color="primary" text-color="white">{{
+              item.count
+            }}</q-avatar>
+            {{ item.text }}
+          </q-chip>
+
+          <div v-if="item.actions.length > 0" class="q-gutter-sm">
+            <q-btn
+              v-for="action in item.actions"
+              :key="action.title"
+              color="primary"
+              size="sm"
+              no-caps
+              type="a"
+              :label="action.title"
+              :href="action.url"
+              ><q-tooltip>{{ action.description }}</q-tooltip></q-btn
+            >
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-btn-dropdown>
 </template>
 
@@ -19,9 +61,37 @@ export default {
     tooltip: { type: String, required: false, default: '' },
     icon: { type: String, required: false, default: '' }
   },
+  data() {
+    return {
+      relations: [],
+      loading: false
+    }
+  },
   computed: {
     link() {
       return `/${this.model}/results/${this.pk}`
+    }
+  },
+  methods: {
+    async getRelations() {
+      const url = `/api/v1/token/${this.model}/${this.pk}/relations/`
+
+      console.log(this.relations)
+      if (this.relations.length > 0) return
+
+      this.loading = true
+      await this.$axios
+        .get(url)
+        .then((response) => {
+          this.relations = response.data
+        })
+        .catch((error) => {
+          this.$store.dispatch(
+            'ui/notifyError',
+            error.response.data.detail || error.response.data
+          )
+        })
+      this.loading = false
     }
   }
 }
