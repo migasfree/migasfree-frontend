894200b015676caf5d31d5a0496a92e59ee6ee6f
jact <jachavar@gmail.com>
2020-11-11 12:13:56 +0100

    Added Breadcrums component

---
+++ src/components/ui/Breadcrumbs.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-breadcrumbs>
+    <q-breadcrumbs-el
+      v-for="(item, index) in items"
+      :key="index"
+      :icon="item.icon"
+      :to="goTo(item.to)"
+      :label="item.text"
+    />
+  </q-breadcrumbs>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumbs',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    goTo(to) {
+      if (to) {
+        return { name: to }
+      }
+      return ''
+    }
+  }
+}
+</script>
