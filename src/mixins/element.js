7b586154c2b044e069167b6759eda914e380bb0a
Jose Antonio Chavarría <jachavar@gmail.com>
2020-11-12 14:48:49 +0100

    Added more methods to mixin

--- src/mixins/element.js
+++ src/mixins/element.js
@@ -1,3 +1,5 @@
+const MAC_RAW_LEN = 12
+
 export const elementMixin = {
   methods: {
     elementIcon(status) {
@@ -15,6 +17,45 @@ export const elementMixin = {
         case 'unsubscribed':
           return 'mdi-recycle-variant'
       }
+    },
+
+    productIcon(value) {
+      switch (value) {
+        case 'desktop':
+          return 'mdi-desktop-tower-monitor'
+        case 'laptop':
+          return 'mdi-laptop'
+        case 'virtual':
+          return 'mdi-cube-outline'
+        case 'docker':
+          return 'mdi-docker'
+        default:
+          return 'mdi-help'
+      }
+    },
+
+    cpuIcon(value) {
+      return `mdi-cpu-${value}-bit`
+    },
+
+    chunk(str, n) {
+      const ret = []
+
+      for (let i = 0, len = str.length; i < len; i += n) {
+        ret.push(str.substr(i, n))
+      }
+
+      return ret
+    },
+
+    humanMacAddress(value) {
+      if (!value) return ''
+
+      const addresses = this.chunk(value, MAC_RAW_LEN)
+      for (let i = 0; i < addresses.length; i++) {
+          addresses[i] = this.chunk(addresses[i], 2).join(':')
+      }
+      return addresses.join(', ')
     }
   }
 }
