a0c32b07822f109ea2b814b816bf2fc52d6a7eb8
jact <jachavar@gmail.com>
2020-11-16 14:27:22 +0100

    Added computer events page

---
+++ src/pages/computers/results/events.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <template v-if="computer.id">
+      <div class="row">
+        <div class="col-md">
+          <h2 class="text-h3">
+            Sucesos:
+            <MigasLink
+              model="computers"
+              :pk="computer.id"
+              :value="computer.__str__ || ''"
+              :icon="elementIcon(computer.status)"
+            />
+          </h2>
+        </div>
+      </div>
+    </template>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import MigasLink from 'components/MigasLink'
+import { elementMixin } from 'mixins/element'
+import { dateMixin } from 'mixins/date'
+
+export default {
+  components: { Breadcrumbs, MigasLink },
+  mixins: [elementMixin, dateMixin],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: 'Datos',
+          icon: 'mdi-database-search'
+        },
+        {
+          text: 'Ordenadores',
+          to: 'computers-dashboard'
+        },
+        {
+          text: 'Resultados',
+          to: 'computers-list'
+        },
+        {
+          text: 'Id',
+          to: { name: 'computer-detail', params: { id: 0 } }
+        },
+        {
+          text: 'Sucesos'
+        }
+      ],
+      computer: {}
+    }
+  },
+  async mounted() {
+    await this.$axios
+      .get(`/api/v1/token/computers/${this.$route.params.id}/`)
+      .then((response) => {
+        console.log(response)
+        this.computer = response.data
+        this.breadcrumbs[4].text = this.computer.__str__
+        this.breadcrumbs[4].to.params.id = this.computer.id
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error.response.data.detail)
+      })
+  }
+}
+</script>
