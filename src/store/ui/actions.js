9714bcb19333dafda96f1b8f58e06fa9867332ca
jact <jachavar@gmail.com>
2020-11-16 13:05:49 +0100

    Added notifySuccess action

--- src/store/ui/actions.js
+++ src/store/ui/actions.js
@@ -6,3 +6,12 @@ export function notifyError(context, message) {
     icon: 'mdi-alert-circle-outline'
   })
 }
+
+export function notifySuccess(context, message) {
+  this._vm.$q.notify({
+    color: 'positive',
+    position: 'bottom',
+    message,
+    icon: 'mdi-check-bold'
+  })
+}
