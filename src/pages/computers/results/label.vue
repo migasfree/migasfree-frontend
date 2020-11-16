b0ea2141fb222314e2782d3e3012224656f0bc02
jact <jachavar@gmail.com>
2020-11-16 13:45:17 +0100

    Added computer label page

---
+++ src/pages/computers/results/label.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-card id="label" flat bordered>
+    <q-list>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img alt="Migasfree logo" src="~assets/migasfree-logo.svg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ label.search }}</q-item-label>
+          <q-item-label caption>
+            {{ label.uuid }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item class="text-center">
+        <q-item-section> Server: {{ server }} </q-item-section>
+      </q-item>
+
+      <q-separator inset />
+
+      <q-item class="text-center">
+        <q-item-section>
+          {{ label.helpdesk }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      label: {},
+      server: process.env.MIGASFREE_SERVER || 'http://localhost'
+    }
+  },
+  async mounted() {
+    await this.$axios
+      .get(`/api/v1/token/computers/${this.$route.params.id}/label`)
+      .then((response) => {
+        console.log(response)
+        this.label = response.data
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error.response.data.detail)
+      })
+  }
+}
+</script>
+
+<style scoped>
+#label {
+  width: 35em;
+  border: 1px solid #000 !important;
+}
+</style>
