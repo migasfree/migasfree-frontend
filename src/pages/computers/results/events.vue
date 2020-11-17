da0acb75c75b7d89562655e0d67f6724c426df18
jact <jachavar@gmail.com>
2020-11-17 21:08:32 +0100

    Improved functionality

--- src/pages/computers/results/events.vue
+++ src/pages/computers/results/events.vue
@@ -28,20 +28,58 @@
       </div>
 
       <div class="row">
-        <q-btn-group>
-          <q-btn label="Sincronizaciones" no-caps />
-          <q-btn label="Errores" no-caps />
-          <q-btn label="Fallas" no-caps />
-          <q-btn label="Registros de Estado" no-caps />
-          <q-btn label="Migraciones" no-caps />
-        </q-btn-group>
+        <div class="col-12">
+          <q-btn-toggle
+            v-model="event"
+            spread
+            no-caps
+            toggle-color="primary"
+            :options="[
+              { slot: 'syncs', value: 'syncs' },
+              { slot: 'errors', value: 'errors' },
+              { slot: 'faults', value: 'faults' },
+              { slot: 'statusLogs', value: 'statusLogs' },
+              { slot: 'migrations', value: 'migrations' }
+            ]"
+            @input="updateEvent"
+          >
+            <template v-slot:syncs>
+              <q-icon name="mdi-sync" />
+              Sincronizaciones ({{ events.syncs.total }})
+            </template>
+
+            <template v-slot:errors>
+              <q-icon name="mdi-bug" />
+              Errores ({{ events.errors.total }})
+            </template>
+
+            <template v-slot:faults>
+              <q-icon name="mdi-bomb" />
+              Fallas ({{ events.faults.total }})
+            </template>
+
+            <template v-slot:statusLogs>
+              <q-icon name="mdi-flag-variant" />
+              Registros de Estado ({{ events.statusLogs.total }})
+            </template>
+
+            <template v-slot:migrations>
+              <q-icon name="mdi-map-marker-right" />
+              Migraciones ({{ events.migrations.total }})
+            </template>
+          </q-btn-toggle>
+        </div>
       </div>
 
       <div class="row">
-        <HeatMap
-          :data="{ data: [] }"
-          :start="showDate(computer.created_at, 'YYYY-MM-DD')"
-        />
+        <div class="col-12">
+          <HeatMap
+            :title="current.title"
+            :data="current.data"
+            :total="current.total"
+            :start="showDate(computer.created_at, 'YYYY-MM-DD')"
+          />
+        </div>
       </div>
     </template>
   </q-page>
@@ -85,7 +123,40 @@ export default {
           text: 'Sucesos'
         }
       ],
-      computer: {}
+      computer: {},
+      event: null,
+      current: {
+        title: '',
+        data: [],
+        total: 0
+      },
+      events: {
+        syncs: {
+          data: [],
+          total: 0,
+          title: 'Sincronizaciones'
+        },
+        errors: {
+          data: [],
+          total: 0,
+          title: 'Errores'
+        },
+        faults: {
+          data: [],
+          total: 0,
+          title: 'Fallas'
+        },
+        migrations: {
+          data: [],
+          total: 0,
+          title: 'Migraciones'
+        },
+        statusLogs: {
+          data: [],
+          total: 0,
+          title: 'Registros de Estado'
+        }
+      }
     }
   },
   async mounted() {
@@ -96,10 +167,96 @@ export default {
         this.computer = response.data
         this.breadcrumbs[4].text = this.computer.__str__
         this.breadcrumbs[4].to.params.id = this.computer.id
+        this.loadItems()
       })
       .catch((error) => {
         this.$store.dispatch('ui/notifyError', error.response.data.detail)
       })
+  },
+  methods: {
+    async loadItems() {
+      const queryString = {
+        computer_id: this.computer.id,
+        start_date: this.showDate(this.computer.created_at, 'YYYY-MM-DD'),
+        end_date: this.showDate(
+          new Date(Date.now()).toISOString(),
+          'YYYY-MM-DD'
+        )
+      }
+
+      await this.$axios
+        .get(`/api/v1/token/stats/syncs/by-day/`, { params: queryString })
+        .then((response) => {
+          this.events.syncs.data = response.data
+          this.events.syncs.total = response.data.reduce(
+            (accumulator, current) => accumulator + parseInt(current[1]),
+            0
+          )
+
+          // default event
+          this.event = 'syncs'
+          this.updateEvent(this.event)
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error.response.data)
+        })
+
+      await this.$axios
+        .get(`/api/v1/token/stats/errors/by-day/`, { params: queryString })
+        .then((response) => {
+          this.events.errors.data = response.data
+          this.events.errors.total = response.data.reduce(
+            (accumulator, current) => accumulator + parseInt(current[1]),
+            0
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error.response.data)
+        })
+
+      await this.$axios
+        .get(`/api/v1/token/stats/faults/by-day/`, { params: queryString })
+        .then((response) => {
+          this.events.faults.data = response.data
+          this.events.faults.total = response.data.reduce(
+            (accumulator, current) => accumulator + parseInt(current[1]),
+            0
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error.response.data)
+        })
+
+      await this.$axios
+        .get(`/api/v1/token/stats/migrations/by-day/`, { params: queryString })
+        .then((response) => {
+          this.events.migrations.data = response.data
+          this.events.migrations.total = response.data.reduce(
+            (accumulator, current) => accumulator + parseInt(current[1]),
+            0
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error.response.data)
+        })
+
+      await this.$axios
+        .get(`/api/v1/token/stats/status-logs/by-day/`, { params: queryString })
+        .then((response) => {
+          this.events.statusLogs.data = response.data
+          this.events.statusLogs.total = response.data.reduce(
+            (accumulator, current) => accumulator + parseInt(current[1]),
+            0
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error.response.data)
+        })
+    },
+
+    updateEvent(evt) {
+      this.current = this.events[evt]
+    }
   }
 }
 </script>
