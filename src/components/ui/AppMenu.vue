071db052b2906c36b54709bf714ef4f2c6dd5316
jact <jachavar@gmail.com>
2020-11-18 11:21:21 +0100

    Added icon to items.options

--- src/components/ui/AppMenu.vue
+++ src/components/ui/AppMenu.vue
@@ -8,6 +8,9 @@
     >
       <q-list v-for="option in item.options" :key="option.to">
         <q-item clickable :to="{ name: option.to }" :inset-level="1">
+          <q-item-section v-if="option.icon">
+            <q-icon :name="option.icon" size="sm" />
+          </q-item-section>
           <q-item-section>{{ option.title }}</q-item-section>
         </q-item>
         <q-separator v-if="option.separatorAfter" />
@@ -86,7 +89,11 @@ export default {
           icon: 'mdi-database-search',
           title: 'Datos',
           options: [
-            { title: 'Ordenadores', to: 'computers-dashboard' },
+            {
+              title: 'Ordenadores',
+              to: 'computers-dashboard',
+              icon: 'mdi-desktop-classic'
+            }
             /* { title: 'Reemplazo de Ordenadores', to: 'computers-replacement' },
             {
               title: 'Usuarios/as',
@@ -95,13 +102,14 @@ export default {
             },
             { title: 'Atributos', to: 'attributes-dashboard' },
             { title: 'Etiquetas', to: 'tags-dashboard', separatorAfter: true },
-            { title: 'Sincronizaciones', to: 'synchronizations-dashboard' },
-            { title: 'Errores', to: 'errors-dashboard' },
-            { title: 'Fallas', to: 'faults-dashboard' },
-            { title: 'Registros de Estados', to: 'status-logs-dashboard' },
+            { title: 'Sincronizaciones', to: 'synchronizations-dashboard', icon: 'mdi-sync' },
+            { title: 'Errores', to: 'errors-dashboard', icon: 'mdi-bug' },
+            { title: 'Fallas', to: 'faults-dashboard', icon: 'mdi-bomb' },
+            { title: 'Registros de Estados', to: 'status-logs-dashboard', icon: 'mdi-flag-variant' },
             {
               title: 'Migraciones',
               to: 'migrations-dashboard',
+              icon: 'mdi-map-marker-right',
               separatorAfter: true
             },
             { title: 'Notificaciones', to: 'notifications-dashboard' } */
