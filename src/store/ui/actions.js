e70fe5ad7993f3c825cbe52427ac463069388820
jact <jachavar@gmail.com>
2020-11-19 19:49:12 +0100

    notifyError accepts object or string (https://medium.com/@danielalvidrez/handling-error-responses-with-grace-b6fd3c5886f0)

--- src/store/ui/actions.js
+++ src/store/ui/actions.js
@@ -1,4 +1,46 @@
-export function notifyError(context, message) {
+export function notifyError(context, error) {
+  let message = ''
+
+  // Setup Error Message
+  if (typeof error !== 'undefined') {
+    if (error.hasOwnProperty('message')) {
+      message = error.message
+    }
+  }
+
+  if (typeof error.response !== 'undefined') {
+    // Setup Generic Response Messages
+    if (error.response.status === 401) {
+      message = 'UnAuthorized'
+      // vm.$emit('logout') // Emit Logout Event
+    } else if (error.response.status === 404) {
+      message = 'API Route is Missing or Undefined'
+    } else if (error.response.status === 405) {
+      message = 'API Route Method Not Allowed'
+    } else if (error.response.status === 422) {
+      // Validation Message
+    } else if (error.response.status >= 500) {
+      message = 'Server Error'
+    }
+
+    // Try to Use the Response Message
+    if (
+      error.hasOwnProperty('response') &&
+      error.response.hasOwnProperty('data')
+    ) {
+      if (
+        error.response.data.hasOwnProperty('message') &&
+        error.response.data.message.length > 0
+      ) {
+        message = error.response.data.message
+      }
+    }
+  }
+
+  if (typeof error === 'string'){
+    message = error
+  }
+
   this._vm.$q.notify({
     color: 'negative',
     position: 'bottom',
