9fd26bf0fea34a26b06ddae47ea8c4db58de580d
jact <jachavar@gmail.com>
2020-11-20 10:00:33 +0100

    Renamed CrudHeading to Header component

---
+++ src/components/ui/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <q-item class="col-12">
+      <q-item-section v-if="hasAddButton" avatar>
+        <q-btn
+          fab-mini
+          dark
+          color="primary"
+          icon="mdi-plus"
+          @click="$emit('new')"
+        />
+      </q-item-section>
+      <q-item-section>
+        <h2 class="text-h3">
+          {{ title }} <q-chip v-if="results !== null">{{ results }}</q-chip>
+        </h2>
+      </q-item-section>
+    </q-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: { type: String, required: true },
+    results: { type: Number, required: false, default: null },
+    hasAddButton: { type: Boolean, required: false, default: true }
+  }
+}
+</script>
