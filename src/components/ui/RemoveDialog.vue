15425ae0254764f2c891c77e47678a39f1ddb052
jact <jachavar@gmail.com>
2020-11-20 17:54:44 +0100

    Added RemoveDialog component

---
+++ src/components/ui/RemoveDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-dialog v-model="value" persistent>
+    <q-card>
+      <q-card-section class="row items-center">
+        <q-avatar icon="mdi-alert-remove" color="negative" text-color="white" />
+        <span class="q-ml-sm">{{ message }}</span>
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn v-close-popup flat label="Cancel" color="primary" />
+        <q-btn
+          v-close-popup
+          label="Borrar"
+          icon="mdi-delete"
+          color="negative"
+          @click="$emit('confirmed')"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'RemoveDialog',
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
