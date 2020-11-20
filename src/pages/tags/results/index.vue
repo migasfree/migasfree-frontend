1e58b0c32fe31758a332452d224dd9f4fbd8a401
jact <jachavar@gmail.com>
2020-11-20 13:32:57 +0100

    Added tags list page

---
+++ src/pages/tags/results/index.vue
@@ -0,0 +1,274 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header
+      title="Etiquetas"
+      :results="totalRecords"
+      @new="$router.push({ name: 'tag-add' })"
+    />
+
+    <SearchFilter
+      v-model="tableFilters.search"
+      @search="onSearch"
+      @clear="onSearchClear"
+    />
+
+    <div class="row q-pa-md">
+      <div class="col-12">
+        <q-btn @click="resetFilters">Reset all filters</q-btn>
+      </div>
+    </div>
+
+    <vue-good-table
+      ref="myTable"
+      :columns="columns"
+      :rows="rows"
+      mode="remote"
+      compact-mode
+      :total-rows="totalRecords"
+      :is-loading.sync="isLoading"
+      :line-numbers="false"
+      :select-options="selectOptions"
+      :pagination-options="paginationOptions"
+      :search-options="searchOptions"
+      style-class="vgt-table striped condensed"
+      @on-page-change="onPageChange"
+      @on-sort-change="onSortChange"
+      @on-column-filter="onColumnFilter"
+      @on-per-page-change="onPerPageChange"
+      @on-selected-rows-change="onSelectionChanged"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'actions'">
+          <q-btn
+            round
+            size="sm"
+            icon="mdi-pencil"
+            color="primary"
+            @click="edit(props.row.id)"
+          />
+          <q-btn
+            round
+            size="sm"
+            icon="mdi-delete"
+            color="negative"
+            @click="remove(props.row.id)"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'value'">
+          <MigasLink
+            model="tags"
+            :pk="props.row.id"
+            :icon="elementIcon(props.row.property_att.prefix)"
+            :value="attributeValue(props.row)"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'property_att'">
+          <MigasLink
+            model="stamps"
+            :pk="props.row.property_att.id"
+            :value="props.row.property_att.name || ''"
+          />
+        </span>
+        <span v-else>
+          {{ props.formattedRow[props.column.field] }}
+        </span>
+      </template>
+      <div slot="emptystate">{{ $t('vgt.noData') }}</div>
+      <div slot="selected-row-actions">
+        <q-btn size="sm" color="negative" icon="mdi-delete"></q-btn>
+      </div>
+    </vue-good-table>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import SearchFilter from 'components/ui/SearchFilter'
+import Header from 'components/ui/Header'
+import MigasLink from 'components/MigasLink'
+import { elementMixin } from 'mixins/element'
+import { datagridMixin } from 'mixins/datagrid'
+
+export default {
+  components: {
+    Breadcrumbs,
+    SearchFilter,
+    Header,
+    MigasLink
+  },
+  mixins: [elementMixin, datagridMixin],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: 'Datos',
+          icon: 'mdi-database-search'
+        },
+        {
+          text: 'Etiquetas',
+          icon: 'mdi-tag',
+          to: 'tags-dashboard'
+        },
+        {
+          text: 'Resultados'
+        }
+      ],
+      columns: [
+        {
+          field: 'id',
+          hidden: true
+        },
+        {
+          label: 'Actions',
+          field: 'actions',
+          html: true,
+          sortable: false,
+          globalSearchDisabled: true
+        },
+        {
+          label: 'Etiqueta',
+          field: 'value',
+          html: true,
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: 'Description',
+          field: 'description',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: 'Categoría de etiqueta',
+          field: 'property_att',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.all'),
+            trigger: 'enter'
+          }
+        }
+      ]
+    }
+  },
+  created() {
+    if (this.$route.query.property_id) {
+      this.updateParams({
+        columnFilters: { property_att: this.$route.query.property_id }
+      })
+      this.columns[4].filterOptions.filterValue = this.$route.query.property_id
+    }
+
+    if (this.$route.query.search) {
+      this.updateParams({
+        columnFilters: { search: this.$route.query.search }
+      })
+      this.tableFilters.search = this.$route.query.search
+    }
+  },
+  methods: {
+    onSearch(value) {
+      this.tableFilters.search = value
+      console.log(this.tableFilters.search)
+      this.updateParams({
+        columnFilters: { search: this.tableFilters.search }
+      })
+      this.loadItems()
+    },
+
+    onSearchClear() {
+      this.onSearch('')
+    },
+
+    paramsToQueryString() {
+      let ret = `page_size=${this.serverParams.perPage}&page=${this.serverParams.page}`
+
+      if (Object.keys(this.serverParams.columnFilters).length) {
+        ret +=
+          '&' +
+          Object.entries(this.serverParams.columnFilters)
+            .map(([key, val]) => {
+              switch (key) {
+                case 'property_att':
+                  return `property_att__id=${val}`
+                case 'search':
+                  return `${key}=${val}`
+                default:
+                  return `${key.replace('.', '__')}__icontains=${val}`
+              }
+            })
+            .join('&')
+      }
+
+      if (this.serverParams.sort.field) {
+        ret += `&ordering=${this.serverParams.sort.type}${this.serverParams.sort.field}`
+      }
+
+      console.log(ret)
+      return ret
+    },
+
+    async loadFilters() {
+      await this.$axios
+        .get('/api/v1/token/stamps/')
+        .then((response) => {
+          console.log(response)
+          this.columns[4].filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    async loadItems() {
+      this.isLoading = true
+      await this.$axios
+        .get('/api/v1/token/tags/?' + this.paramsToQueryString())
+        .then((response) => {
+          console.log(response)
+          this.totalRecords = response.data.count
+          this.rows = response.data.results
+          this.isLoading = false
+        })
+        .catch((error) => {
+          this.isLoading = false
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    resetFilters() {
+      this.$refs.myTable.reset()
+      this.updateParams({ columnFilters: {} })
+      this.tableFilters.search = ''
+      this.loadItems()
+    },
+
+    edit(id) {
+      this.$router.push({ name: 'tag-detail', params: { id } })
+    },
+
+    remove(id) {
+      console.log(id)
+    }
+  }
+}
+</script>
