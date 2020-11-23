8d1b3417a17e62fea0ff210940de13092614336a
jact <jachavar@gmail.com>
2020-11-23 16:05:07 +0100

    Added MIGASFREE_CHART_COLORS constant

--- src/config/app.conf.js
+++ src/config/app.conf.js
@@ -1,3 +1,19 @@
-export default {
-  MIGASFREE_SECONDS_MESSAGE_ALERT: 1800
-}
+export const MIGASFREE_SECONDS_MESSAGE_ALERT = 1800
+
+export const MIGASFREE_CHART_COLORS = [
+  '#7cb5ec',
+  '#434348',
+  '#90ed7d',
+  '#f7a35c',
+  '#8085e9',
+  '#f15c80',
+  '#e4d354',
+  '#2b908f',
+  '#f45b5b',
+  '#91e8e1',
+  '#2f7ed8',
+  '#0d233a',
+  '#8bbc21',
+  '#910000',
+  '#1aadce'
+]
