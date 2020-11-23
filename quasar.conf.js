985b8ac2d32fd18ff1ae5ef9777dfa61107f3545
jact <jachavar@gmail.com>
2020-11-23 11:01:31 +0100

    Added pluralize boot file

--- quasar.conf.js
+++ quasar.conf.js
@@ -20,7 +20,7 @@ module.exports = function(/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
-    boot: ['i18n', 'axios', 'vue-good-table', 'vue-echarts'],
+    boot: ['i18n', 'axios', 'vue-good-table', 'vue-echarts', 'pluralize'],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ['app.sass'],
