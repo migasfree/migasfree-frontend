29e1a838e8deab1a2c45d19153ed6e3739ff9820
jact <jachavar@gmail.com>
2020-11-23 15:37:50 +0100

    Changed dashboard content

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,15 +1,114 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Migasfree logo"
-      src="~assets/migasfree-logo.svg"
-      width="300"
-    >
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header title="Cuadro de mando" :has-add-button="false" />
+
+    <div class="row">
+      <div class="col-md">
+        <NestedPieChart
+          title="Ordenadores productivos"
+          :data="productiveComputers"
+          :url="productiveUrl"
+          @getLink="goTo"
+        />
+      </div>
+
+      <div class="col-md">
+        <NestedPieChart
+          title="Errores sin comprobar"
+          :data="uncheckedErrors"
+          :url="uncheckedErrorsUrl"
+          @getLink="goTo"
+        />
+      </div>
+
+      <div class="col-md">
+        <NestedPieChart
+          title="Fallas sin comprobar"
+          :data="uncheckedFaults"
+          :url="uncheckedFaultsUrl"
+          @getLink="goTo"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import Header from 'components/ui/Header'
+import NestedPieChart from 'components/chart/NestedPie'
+
 export default {
-  name: 'PageIndex'
+  components: { Breadcrumbs, Header, NestedPieChart },
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          icon: 'mdi-home'
+        }
+      ],
+      productiveComputers: {},
+      uncheckedErrors: {},
+      uncheckedFaults: {}
+    }
+  },
+  computed: {
+    productiveUrl() {
+      return {
+        name: 'computers-list',
+        query: { status_in: 'intended,reserved,unknown' }
+      }
+    },
+
+    uncheckedErrorsUrl() {
+      return {
+        name: 'errors-list',
+        query: { checked: false }
+      }
+    },
+
+    uncheckedFaultsUrl() {
+      return {
+        name: 'faults-list',
+        query: { checked: false }
+      }
+    }
+  },
+  async mounted() {
+    await this.$axios
+      .get('/api/v1/token/stats/computers/productive/platform/')
+      .then((response) => {
+        this.productiveComputers = response.data
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
+
+    await this.$axios
+      .get('/api/v1/token/stats/errors/unchecked/')
+      .then((response) => {
+        this.uncheckedErrors = response.data
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
+
+    await this.$axios
+      .get('/api/v1/token/stats/faults/unchecked/')
+      .then((response) => {
+        this.uncheckedFaults = response.data
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
+  },
+  methods: {
+    goTo(params) {
+      console.log(params)
+    }
+  }
 }
 </script>
