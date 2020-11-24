474d7cac3d3e7c4d15f288c2aec1b6dc6342bb1a
Jose Antonio Chavarría <jachavar@gmail.com>
2020-11-24 11:43:24 +0100

    Changed color palette (coolors.co)

--- src/config/app.conf.js
+++ src/config/app.conf.js
@@ -1,19 +1,19 @@
 export const MIGASFREE_SECONDS_MESSAGE_ALERT = 1800
 
 export const MIGASFREE_CHART_COLORS = [
-  '#7cb5ec',
-  '#434348',
-  '#90ed7d',
-  '#f7a35c',
-  '#8085e9',
-  '#f15c80',
-  '#e4d354',
-  '#2b908f',
-  '#f45b5b',
-  '#91e8e1',
-  '#2f7ed8',
-  '#0d233a',
-  '#8bbc21',
-  '#910000',
-  '#1aadce'
+  // coolors.co
+  '#294C60', // charcoal
+  '#ff8552', // coral
+  '#297373', // skobeloff
+  '#DB3A34', // cg red
+  '#85FFC7', // aquamarine
+  '#39393A', // onyx
+  '#FFC857', // maximum yellow red
+  '#613A3A', // rose ebony
+  '#084C61', // midnight green eagle green
+  '#573280', // spanish violet
+  '#ADA8B6', // silver metallic
+  '#845A6D', // mauve taupe
+  '#3D3B8E', // dark slate blue
+  '#FFC49B' // peach crayola
 ]
