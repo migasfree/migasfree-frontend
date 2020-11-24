402f178ed074482268c429b03b6d3b5d8f07c945
Jose Antonio Chavarría <jachavar@gmail.com>
2020-11-24 14:31:18 +0100

    Changed (again) color palette

--- src/config/app.conf.js
+++ src/config/app.conf.js
@@ -1,5 +1,6 @@
 export const MIGASFREE_SECONDS_MESSAGE_ALERT = 1800
 
+/*
 export const MIGASFREE_CHART_COLORS = [
   // coolors.co
   '#294C60', // charcoal
@@ -16,4 +17,21 @@ export const MIGASFREE_CHART_COLORS = [
   '#845A6D', // mauve taupe
   '#3D3B8E', // dark slate blue
   '#FFC49B' // peach crayola
+]
+*/
+
+// https://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12
+export const MIGASFREE_CHART_COLORS = [
+  '#a6cee3',
+  '#1f78b4',
+  '#b2df8a',
+  '#33a02c',
+  '#fb9a99',
+  '#e31a1c',
+  '#fdbf6f',
+  '#ff7f00',
+  '#cab2d6',
+  '#6a3d9a',
+  '#ffff99',
+  '#b15928'
 ]
