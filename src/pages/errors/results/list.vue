eef58e553b7a6289ea23049e6a115206418321b6
Jose Antonio Chavarría <jachavar@gmail.com>
2020-11-26 14:54:04 +0100

    Added errors list page

---
+++ src/pages/errors/results/list.vue
@@ -0,0 +1,276 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header title="Errores" :results="totalRecords" :has-add-button="false" />
+
+    <SearchFilter
+      v-model="tableFilters.search"
+      @search="onSearch"
+      @clear="onSearchClear"
+    />
+
+    <div class="row q-pa-md">
+      <div class="col-12">
+        <q-btn @click="resetFilters">Reset all filters</q-btn>
+      </div>
+    </div>
+
+    <vue-good-table
+      ref="myTable"
+      :columns="columns"
+      :rows="rows"
+      mode="remote"
+      compact-mode
+      :total-rows="totalRecords"
+      :is-loading.sync="isLoading"
+      :line-numbers="false"
+      :select-options="selectOptions"
+      :pagination-options="paginationOptions"
+      :search-options="searchOptions"
+      style-class="vgt-table striped condensed"
+      @on-page-change="onPageChange"
+      @on-sort-change="onSortChange"
+      @on-column-filter="onColumnFilter"
+      @on-per-page-change="onPerPageChange"
+      @on-selected-rows-change="onSelectionChanged"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'actions'">
+          <q-btn
+            round
+            size="sm"
+            icon="mdi-pencil"
+            color="primary"
+            @click="edit(props.row.id)"
+          />
+          <q-btn
+            round
+            size="sm"
+            icon="mdi-delete"
+            color="negative"
+            @click="confirmRemove(props.row.id)"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'computer.cid_description'">
+          <MigasLink
+            model="computers"
+            :pk="props.row.computer.id"
+            :value="props.row.computer.cid_description"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'project.name'">
+          <MigasLink
+            model="projects"
+            :pk="props.row.project.id"
+            :value="props.row.project.name || ''"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'created_at'">
+          {{ showDate(props.row.created_at) }}
+        </span>
+        <span v-else-if="props.column.field == 'description'">
+          <pre class="ellipsis-3-lines">{{ props.row.description }}</pre>
+        </span>
+        <span v-else-if="props.column.field == 'checked'">
+          <q-icon
+            v-if="props.row.checked"
+            name="mdi-check-bold"
+            color="positive"
+            size="md"
+            ><q-tooltip>Sí</q-tooltip></q-icon
+          >
+          <q-icon v-else name="mdi-close-thick" color="negative" size="md">
+            <q-tooltip>No</q-tooltip>
+          </q-icon>
+        </span>
+        <span v-else>
+          {{ props.formattedRow[props.column.field] }}
+        </span>
+      </template>
+      <div slot="emptystate">{{ $t('vgt.noData') }}</div>
+      <div slot="selected-row-actions">
+        <q-btn
+          size="sm"
+          color="negative"
+          icon="mdi-delete"
+          @click="confirmRemove"
+        ></q-btn>
+      </div>
+    </vue-good-table>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import SearchFilter from 'components/ui/SearchFilter'
+import Header from 'components/ui/Header'
+import MigasLink from 'components/MigasLink'
+import { dateMixin } from 'mixins/date'
+import { elementMixin } from 'mixins/element'
+import { datagridMixin } from 'mixins/datagrid'
+
+export default {
+  components: {
+    Breadcrumbs,
+    SearchFilter,
+    Header,
+    MigasLink
+  },
+  mixins: [dateMixin, elementMixin, datagridMixin],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: 'Datos',
+          icon: 'mdi-database-search'
+        },
+        {
+          text: 'Errores',
+          icon: 'mdi-bug',
+          to: 'errors-dashboard'
+        },
+        {
+          text: 'Resultados'
+        }
+      ],
+      columns: [
+        {
+          field: 'id',
+          hidden: true
+        },
+        {
+          label: 'Actions',
+          field: 'actions',
+          html: true,
+          sortable: false,
+          globalSearchDisabled: true
+        },
+        {
+          label: 'Fecha',
+          field: 'created_at'
+        },
+        {
+          field: 'computer.id',
+          hidden: true
+        },
+        {
+          label: 'Ordenador',
+          field: 'computer.cid_description'
+        },
+        {
+          field: 'project.id',
+          hidden: true
+        },
+        {
+          label: 'Proyecto',
+          field: 'project.name'
+        },
+        {
+          label: 'Comprobado',
+          field: 'checked'
+        },
+        {
+          label: 'Descripción',
+          field: 'description',
+          html: true
+        }
+      ]
+    }
+  },
+  created() {
+    if (this.$route.query.search) {
+      this.updateParams({
+        columnFilters: { search: this.$route.query.search }
+      })
+      this.tableFilters.search = this.$route.query.search
+    }
+  },
+  methods: {
+    onSearch(value) {
+      this.tableFilters.search = value
+      console.log(this.tableFilters.search)
+      this.updateParams({
+        columnFilters: { search: this.tableFilters.search }
+      })
+      this.loadItems()
+    },
+
+    onSearchClear() {
+      this.onSearch('')
+    },
+
+    paramsToQueryString() {
+      let ret = `page_size=${this.serverParams.perPage}&page=${this.serverParams.page}`
+
+      if (Object.keys(this.serverParams.columnFilters).length) {
+        ret +=
+          '&' +
+          Object.entries(this.serverParams.columnFilters)
+            .map(([key, val]) => {
+              switch (key) {
+                case 'property_att':
+                  return `property_att__id=${val}`
+                case 'search':
+                  return `${key}=${val}`
+                default:
+                  return `${key.replace('.', '__')}__icontains=${val}`
+              }
+            })
+            .join('&')
+      }
+
+      if (this.serverParams.sort.field) {
+        ret += `&ordering=${this.serverParams.sort.type}${this.serverParams.sort.field}`
+      }
+
+      console.log(ret)
+      return ret
+    },
+
+    async loadFilters() {},
+
+    async loadItems() {
+      this.isLoading = true
+      await this.$axios
+        .get('/api/v1/token/errors/?' + this.paramsToQueryString())
+        .then((response) => {
+          console.log(response)
+          this.totalRecords = response.data.count
+          this.rows = response.data.results
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+        .finally(() => (this.loading = false))
+    },
+
+    resetFilters() {
+      this.$refs.myTable.reset()
+      this.updateParams({ columnFilters: {} })
+      this.tableFilters.search = ''
+      this.loadItems()
+    },
+
+    edit(id) {
+      this.$router.push({ name: 'error-detail', params: { id } })
+    },
+
+    remove(id) {
+      this.$axios
+        .delete(`/api/v1/token/errors/${id}/`)
+        .then((response) => {
+          this.$store.dispatch('ui/notifySuccess', 'Item deleted!')
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    }
+  }
+}
+</script>
