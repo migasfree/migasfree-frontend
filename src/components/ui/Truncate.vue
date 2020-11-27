01f895e1acada3902a5d9c4f04be3c6bb8188a44
jact <jachavar@gmail.com>
2020-11-27 09:12:42 +0100

    Added text-clipper dependency (for Truncate component)

---
+++ src/components/ui/Truncate.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <template v-if="value.length <= len">
+      <pre>{{ value }}</pre>
+    </template>
+    <q-list v-else bordered>
+      <q-expansion-item>
+        <template v-slot:header>
+          <pre class="overflow">{{ truncatedValue }}</pre>
+        </template>
+        <pre>{{ value }}</pre>
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import clip from 'text-clipper'
+
+export default {
+  name: 'Truncate',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    len: {
+      type: Number,
+      required: false,
+      default: 250
+    }
+  },
+  computed: {
+    truncatedValue() {
+      return clip(this.value, this.len, {
+        html: true,
+        maxLines: 3
+      })
+    }
+  }
+}
+</script>
