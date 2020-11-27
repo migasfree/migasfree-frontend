4e02c7ce8128d595b2d5f1d02937db012ccc43f6
jact <jachavar@gmail.com>
2020-11-27 09:12:05 +0100

    Added BooleanView component

---
+++ src/components/ui/BooleanView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <q-icon v-if="value" name="mdi-check-bold" color="positive" size="md"
+      ><q-tooltip>Sí</q-tooltip></q-icon
+    >
+    <q-icon v-else name="mdi-close-thick" color="negative" size="md">
+      <q-tooltip>No</q-tooltip>
+    </q-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BooleanView',
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
