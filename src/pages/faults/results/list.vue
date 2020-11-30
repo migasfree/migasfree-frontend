9d1eff5e83bf45c5fc23c8685fd67294e16e4841
jact <jachavar@gmail.com>
2020-11-30 13:06:14 +0100

    Added faults list page

---
+++ src/pages/faults/results/list.vue
@@ -0,0 +1,617 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header title="Fallas" :results="totalRecords" :has-add-button="false" />
+
+    <q-list bordered>
+      <q-expansion-item icon="mdi-filter" label="More Filters">
+        <SearchFilter
+          v-model="tableFilters.search"
+          @search="onSearch"
+          @clear="onSearchClear"
+        />
+
+        <div class="row q-pa-md q-col-gutter-lg">
+          <div class="col-sm-6 col-md-4">
+            <q-select
+              v-model="tableFilters.platform.selected"
+              :options="tableFilters.platform.items"
+              label="Por plataforma"
+              dense
+              outlined
+              option-value="id"
+              option-label="name"
+              @input="onPlatformFilter"
+            >
+              <template v-slot:before>
+                <q-icon name="mdi-filter" />
+              </template>
+            </q-select>
+          </div>
+
+          <div class="col-md-4 col-sm-6">
+            <q-select
+              v-model="tableFilters.user.selected"
+              :options="tableFilters.user.items"
+              label="Por asignación"
+              dense
+              outlined
+              option-value="id"
+              option-label="name"
+              @input="onUserFilter"
+            >
+              <template v-slot:before>
+                <q-icon name="mdi-filter" />
+              </template>
+            </q-select>
+          </div>
+
+          <div class="col-sm-6 col-md-4">
+            <DateRangeInput
+              ref="createdAtRange"
+              v-model="tableFilters.createdAt.selected"
+              prepend-icon="mdi-filter"
+              label="Por fecha de alta (rango)"
+              @select="onCreatedAtFilter"
+            />
+          </div>
+        </div>
+
+        <div class="row q-pa-md">
+          <div class="col-12">
+            <q-btn @click="resetFilters">Reset all filters</q-btn>
+          </div>
+        </div>
+      </q-expansion-item>
+    </q-list>
+
+    <vue-good-table
+      ref="myTable"
+      :columns="columns"
+      :rows="rows"
+      mode="remote"
+      compact-mode
+      :total-rows="totalRecords"
+      :is-loading.sync="isLoading"
+      :line-numbers="false"
+      :select-options="selectOptions"
+      :pagination-options="paginationOptions"
+      :search-options="searchOptions"
+      style-class="vgt-table striped condensed"
+      @on-page-change="onPageChange"
+      @on-sort-change="onSortChange"
+      @on-column-filter="onColumnFilter"
+      @on-per-page-change="onPerPageChange"
+      @on-selected-rows-change="onSelectionChanged"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'actions'">
+          <q-btn
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-pencil"
+            color="primary"
+            @click="edit(props.row.id)"
+          />
+          <q-btn
+            v-if="!props.row.checked"
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-eye-check"
+            color="positive"
+            @click="updateChecked(props.row.id, true)"
+            ><q-tooltip>Comprobar</q-tooltip></q-btn
+          >
+          <q-btn
+            v-if="props.row.checked"
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-eye-remove"
+            color="negative"
+            @click="updateChecked(props.row.id, false)"
+            ><q-tooltip>No comprobar</q-tooltip></q-btn
+          >
+          <q-btn
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-delete"
+            color="negative"
+            @click="confirmRemove(props.row.id)"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'computer.__str__'">
+          <MigasLink
+            model="computers"
+            :pk="props.row.computer.id"
+            :value="props.row.computer.__str__"
+            :icon="elementIcon(props.row.computer.status)"
+            :tooltip="props.row.computer.summary"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'project.name'">
+          <MigasLink
+            model="projects"
+            :pk="props.row.project.id"
+            :value="props.row.project.name || ''"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'fault_definition.name'">
+          <MigasLink
+            model="fault-definitions"
+            :pk="props.row.fault_definition.id"
+            :value="props.row.fault_definition.name || ''"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'created_at'">
+          {{ showDate(props.row.created_at) }}
+        </span>
+        <span v-else-if="props.column.field == 'result'">
+          <Truncate v-model="props.row.result" />
+        </span>
+        <span v-else-if="props.column.field == 'checked'">
+          <BooleanView v-model="props.row.checked" />
+        </span>
+        <span v-else>
+          {{ props.formattedRow[props.column.field] }}
+        </span>
+      </template>
+      <div slot="emptystate">{{ $t('vgt.noData') }}</div>
+      <div slot="selected-row-actions">
+        <q-btn
+          class="q-ma-xs"
+          size="sm"
+          icon="mdi-eye-check"
+          color="positive"
+          @click="updateItemsChecked(true)"
+          ><q-tooltip>Comprobar</q-tooltip></q-btn
+        >
+        <q-btn
+          class="q-ma-xs"
+          size="sm"
+          icon="mdi-eye-remove"
+          color="negative"
+          @click="updateItemsChecked(false)"
+          ><q-tooltip>No comprobar</q-tooltip></q-btn
+        >
+        <q-btn
+          class="q-ma-xs"
+          size="sm"
+          color="negative"
+          icon="mdi-delete"
+          @click="confirmRemove"
+        ></q-btn>
+      </div>
+    </vue-good-table>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import SearchFilter from 'components/ui/SearchFilter'
+import Header from 'components/ui/Header'
+import BooleanView from 'components/ui/BooleanView'
+import Truncate from 'components/ui/Truncate'
+import DateRangeInput from 'components/ui/DateRangeInput'
+import MigasLink from 'components/MigasLink'
+import { dateMixin } from 'mixins/date'
+import { elementMixin } from 'mixins/element'
+import { datagridMixin } from 'mixins/datagrid'
+
+export default {
+  meta: {
+    title: 'Faults List'
+  },
+  components: {
+    Breadcrumbs,
+    SearchFilter,
+    Header,
+    BooleanView,
+    Truncate,
+    DateRangeInput,
+    MigasLink
+  },
+  mixins: [dateMixin, elementMixin, datagridMixin],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: 'Datos',
+          icon: 'mdi-database-search'
+        },
+        {
+          text: 'Fallas',
+          icon: 'mdi-bug',
+          to: 'faults-dashboard'
+        },
+        {
+          text: 'Resultados'
+        }
+      ],
+      columns: [
+        {
+          field: 'id',
+          hidden: true
+        },
+        {
+          label: 'Actions',
+          field: 'actions',
+          html: true,
+          sortable: false,
+          globalSearchDisabled: true
+        },
+        {
+          label: 'Fecha',
+          field: 'created_at'
+        },
+        {
+          field: 'computer.id',
+          hidden: true
+        },
+        {
+          field: 'computer.status',
+          hidden: true
+        },
+        {
+          field: 'computer.summary',
+          hidden: true
+        },
+        {
+          label: 'Ordenador',
+          field: 'computer.__str__',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          field: 'project.id',
+          hidden: true
+        },
+        {
+          label: 'Proyecto',
+          field: 'project.name',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.all'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: 'Comprobado',
+          field: 'checked',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.all'),
+            trigger: 'enter',
+            filterDropdownItems: [
+              { value: true, text: 'Sí' },
+              { value: false, text: 'No' }
+            ]
+          }
+        },
+        {
+          field: 'fault_definition.id',
+          hidden: true
+        },
+        {
+          label: 'Definición de falla',
+          field: 'fault_definition.name',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.all'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: 'Resultado',
+          field: 'result',
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$t('vgt.filter'),
+            trigger: 'enter'
+          }
+        }
+      ],
+      tableFilters: {
+        search: '',
+        platform: {
+          items: [{ id: '', name: 'Todas' }],
+          selected: null
+        },
+        createdAt: {
+          selected: { from: null, to: null }
+        },
+        user: {
+          items: [{ id: '', name: 'Todos' }],
+          selected: null
+        }
+      }
+    }
+  },
+  created() {
+    if (this.$route.query.platform_id) {
+      this.updateParams({
+        columnFilters: { platform: this.$route.query.platform_id }
+      })
+    }
+
+    if (this.$route.query.project_id) {
+      this.updateParams({
+        columnFilters: { 'project.name': this.$route.query.project_id }
+      })
+      this.columns.find(
+        (x) => x.field === 'project.name'
+      ).filterOptions.filterValue = this.$route.query.project_id
+    }
+
+    if ('checked' in this.$route.query) {
+      this.updateParams({
+        columnFilters: { checked: this.$route.query.checked }
+      })
+      this.columns.find(
+        (x) => x.field === 'checked'
+      ).filterOptions.filterValue = this.$route.query.checked
+    }
+
+    if (this.$route.query.fault_definition_id) {
+      this.updateParams({
+        columnFilters: { fault_definition_id: this.$route.query.fault_definition_id }
+      })
+      this.columns.find(
+        (x) => x.field === 'fault_definition.name'
+      ).filterOptions.filterValue = this.$route.query.fault_definition_id
+    }
+
+    if (this.$route.query.created_at__gte && this.$route.query.created_at__lt) {
+      this.updateParams({
+        columnFilters: {
+          created_at__gte: this.$route.query.created_at__gte,
+          created_at__lt: this.$route.query.created_at__lt
+        }
+      })
+      this.tableFilters.createdAt.selected = {
+        from: this.$route.query.created_at__gte,
+        to: this.$route.query.created_at__lt
+      }
+    }
+
+    if (this.$route.query.search) {
+      this.updateParams({
+        columnFilters: { search: this.$route.query.search }
+      })
+      this.tableFilters.search = this.$route.query.search
+    }
+  },
+  methods: {
+    onSearch(value) {
+      this.tableFilters.search = value
+      console.log(this.tableFilters.search)
+      this.updateParams({
+        columnFilters: { search: this.tableFilters.search }
+      })
+      this.loadItems()
+    },
+
+    onSearchClear() {
+      this.onSearch('')
+    },
+
+    onPlatformFilter(params) {
+      this.updateParams({
+        columnFilters: Object.assign(this.serverParams.columnFilters, {
+          platform: params.id
+        })
+      })
+      this.loadItems()
+    },
+
+    onUserFilter(params) {
+      this.updateParams({
+        columnFilters: Object.assign(this.serverParams.columnFilters, {
+          user: params.id
+        })
+      })
+      this.loadItems()
+    },
+
+    onCreatedAtFilter(params) {
+      console.log(params)
+      console.log('createdAt selected', this.tableFilters.createdAt.selected)
+      this.tableFilters.createdAt.selected = params
+      this.updateParams({
+        columnFilters: Object.assign(this.serverParams.columnFilters, {
+          created_at__gte: this.tableFilters.createdAt.selected.from,
+          created_at__lt: this.tableFilters.createdAt.selected.to
+        })
+      })
+      this.loadItems()
+    },
+
+    paramsToQueryString() {
+      let ret = `page_size=${this.serverParams.perPage}&page=${this.serverParams.page}`
+
+      if (Object.keys(this.serverParams.columnFilters).length) {
+        ret +=
+          '&' +
+          Object.entries(this.serverParams.columnFilters)
+            .map(([key, val]) => {
+              switch (key) {
+                case 'project.name':
+                  return `project__id=${val}`
+                case 'computer.__str__':
+                  return `computer__name__icontains=${val}`
+                case 'platform':
+                  return `project__platform__id=${val}`
+                case 'fault_definition.name':
+                  return `fault_definition_id=${val}`
+                case 'checked':
+                case 'created_at__gte':
+                case 'created_at__lt':
+                case 'user':
+                case 'search':
+                  return `${key}=${val}`
+                default:
+                  return `${key.replace('.', '__')}__icontains=${val}`
+              }
+            })
+            .join('&')
+      }
+
+      if (this.serverParams.sort.field) {
+        ret += `&ordering=${this.serverParams.sort.type}${this.serverParams.sort.field}`
+      }
+
+      console.log(ret)
+      return ret
+    },
+
+    async loadFilters() {
+      await this.$axios
+        .get('/api/v1/token/platforms/')
+        .then((response) => {
+          console.log('plataformas', response.data.results)
+          this.tableFilters.platform.items = this.tableFilters.platform.items.concat(
+            response.data.results
+          )
+
+          if (this.$route.query.platform_id) {
+            this.tableFilters.platform.selected = this.tableFilters.platform.items.find(
+              (x) => x.id == this.$route.query.platform_id
+            )
+          }
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      await this.$axios
+        .get('/api/v1/token/projects/')
+        .then((response) => {
+          console.log(response)
+          this.columns.find(
+            (x) => x.field === 'project.name'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      await this.$axios
+        .get('/api/v1/token/fault-definitions/')
+        .then((response) => {
+          console.log(response)
+          this.columns.find(
+            (x) => x.field === 'fault_definition.name'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      await this.$axios
+        .get('/api/v1/token/faults/user/')
+        .then((response) => {
+          console.log(response)
+          Object.entries(response.data.choices).map(([key, val]) => {
+            this.tableFilters.user.items.push({ id: key, name: val })
+          })
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    async loadItems() {
+      if (this.isLoading) return
+
+      this.isLoading = true
+      await this.$axios
+        .get('/api/v1/token/faults/?' + this.paramsToQueryString())
+        .then((response) => {
+          console.log(response)
+          this.totalRecords = response.data.count
+          this.rows = response.data.results
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+        .finally(() => (this.loading = false))
+    },
+
+    resetFilters() {
+      this.$refs.myTable.reset()
+      this.updateParams({ columnFilters: {} })
+      this.tableFilters.search = ''
+      this.tableFilters.platform.selected = null
+      this.tableFilters.user.selected = null
+
+      this.tableFilters.createdAt.selected = { from: null, to: null }
+      this.$refs.createdAtRange.reset()
+
+      this.loadItems()
+    },
+
+    edit(id) {
+      this.$router.push({ name: 'fault-detail', params: { id } })
+    },
+
+    remove(id, reload = true) {
+      this.$axios
+        .delete(`/api/v1/token/faults/${id}/`)
+        .then((response) => {
+          this.$store.dispatch('ui/notifySuccess', 'Item deleted!')
+          if (reload) this.loadItems()
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    updateChecked(id, value, reload = true) {
+      this.$axios
+        .patch(`/api/v1/token/faults/${id}/`, { checked: value })
+        .then((response) => {
+          this.$store.dispatch('ui/notifySuccess', 'Changed item check value!')
+          if (reload) this.loadItems()
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    updateItemsChecked(value) {
+      const items = this.selectedRows.map((item) => item.id)
+      if (items.length === 0) return
+
+      items.forEach((id) => {
+        this.updateChecked(id, value, items[items.length - 1] === id)
+      })
+    }
+  }
+}
+</script>
