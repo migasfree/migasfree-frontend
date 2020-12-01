1564aa37be5e2a4228283012afdb73c4df13d42a
Jose Antonio Chavarría <jachavar@gmail.com>
2020-12-01 13:42:48 +0100

    Added platform detail page

---
+++ src/pages/platforms/results/detail.vue
@@ -0,0 +1,240 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <div class="row">
+      <div class="col-md">
+        <h2 class="text-h3">
+          Plataforma:
+          <MigasLink
+            v-if="element.id"
+            model="platforms"
+            :pk="element.id"
+            :value="element.name"
+          />
+        </h2>
+      </div>
+    </div>
+
+    <q-card>
+      <q-card-section>
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-12">
+            <q-input
+              v-model="element.name"
+              outlined
+              label="Nombre"
+              lazy-rules
+              :rules="[(val) => !!val || '* Required']"
+            />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions class="justify-around">
+        <q-btn
+          flat
+          color="primary"
+          label="Grabar y añadir otro"
+          icon="mdi-plus"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('add')"
+        />
+        <q-btn
+          flat
+          color="primary"
+          label="Grabar y seguir editando"
+          icon="mdi-content-save-edit"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement"
+        />
+        <q-btn
+          label="Grabar"
+          color="primary"
+          icon="mdi-content-save-move"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('return')"
+        />
+      </q-card-actions>
+    </q-card>
+
+    <div v-if="$route.params.id && element.id" class="row q-pa-md">
+      <q-btn
+        flat
+        icon="mdi-delete"
+        color="negative"
+        label="Borrar"
+        @click="confirmRemove = true"
+      />
+    </div>
+
+    <RemoveDialog
+      v-model="confirmRemove"
+      @confirmed="remove"
+      @canceled="confirmRemove = !confirmRemove"
+    />
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import MigasLink from 'components/MigasLink'
+import RemoveDialog from 'components/ui/RemoveDialog'
+import { elementMixin } from 'mixins/element'
+
+export default {
+  meta() {
+    return {
+      title: this.title
+    }
+  },
+  components: {
+    Breadcrumbs,
+    RemoveDialog,
+    MigasLink
+  },
+  mixins: [elementMixin],
+  data() {
+    return {
+      title: 'Tag',
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: 'Configuración',
+          icon: 'mdi-cogs'
+        },
+        {
+          text: 'Plataformas',
+          icon: 'mdi-layers',
+          to: 'platforms-list'
+        }
+      ],
+      element: { id: 0 },
+      stamps: [],
+      loading: false,
+      confirmRemove: false
+    }
+  },
+  computed: {
+    isValid() {
+      return (
+        this.element.name !== undefined &&
+        this.element.name !== ''
+      )
+    }
+  },
+  created() {
+    if (this.$route.params.id) {
+      this.breadcrumbs.push({
+        text: 'Resultados',
+        to: 'platforms-list'
+      })
+      this.breadcrumbs.push({
+        text: 'Id'
+      })
+    } else {
+      this.breadcrumbs.push({ text: 'Añadir' })
+    }
+  },
+  async mounted() {
+    if (this.$route.params.id) {
+      await this.$axios
+        .get(`/api/v1/token/platforms/${this.$route.params.id}/`)
+        .then((response) => {
+          console.log(response)
+          this.element = response.data
+          this.breadcrumbs.find(
+            (x) => x.text === 'Id'
+          ).text = this.element.name
+          this.title = `${this.title}: ${this.element.name}`
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    }
+  },
+  methods: {
+    async updateElement(action = null) {
+      if (this.element.id) {
+        this.loading = true
+        await this.$axios
+          .patch(`/api/v1/token/platforms/${this.element.id}/`, {
+            name: this.element.name,
+          })
+          .then((response) => {
+            console.log(response)
+            this.$store.dispatch('ui/notifySuccess', 'Data has been changed!')
+            if (action === 'return') {
+              this.$router.push({ name: 'platforms-list' })
+            } else if (action === 'add') {
+              this.element = { id: 0 }
+              if (this.breadcrumbs.length === 5) this.breadcrumbs.pop()
+              this.breadcrumbs[3].text = 'Añadir'
+              this.$router.push({ name: 'platform-add' })
+              this.title = 'Plataforma'
+            }
+          })
+          .catch((error) => {
+            this.$store.dispatch('ui/notifyError', error)
+          })
+          .finally(() => (this.loading = false))
+      } else {
+        this.loading = true
+        await this.$axios
+          .post('/api/v1/token/platforms/', {
+            name: this.element.name,
+          })
+          .then((response) => {
+            console.log(response)
+            this.element = response.data
+            console.log('element new', this.element)
+            this.$store.dispatch('ui/notifySuccess', 'Data has been added!')
+
+            if (action === 'return') {
+              this.$router.push({ name: 'platforms-list' })
+            } else if (action === 'add') {
+              this.element = { id: 0 }
+              this.$router.push({ name: 'platform-add' })
+            } else {
+              if (this.breadcrumbs.length === 4) {
+                this.breadcrumbs.pop()
+                this.breadcrumbs.push({ text: 'Resultados', to: 'platforms-list' })
+                this.breadcrumbs.push({
+                  text: this.element.name
+                })
+                this.title = `Plataforma: ${this.element.name}`
+              }
+              this.$router.push({
+                name: 'platform-detail',
+                params: { id: this.element.id }
+              })
+            }
+          })
+          .catch((error) => {
+            this.$store.dispatch('ui/notifyError', error)
+          })
+          .finally(() => (this.loading = false))
+      }
+    },
+
+    async remove() {
+      console.log(this.element.id)
+      await this.$axios
+        .delete(`/api/v1/token/platforms/${this.element.id}/`)
+        .then((response) => {
+          this.$router.push({ name: 'platforms-list' })
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    }
+  }
+}
+</script>
