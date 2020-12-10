44366452bc3953763c77d2987f6072a71b1c7629
jact <jachavar@gmail.com>
2020-12-10 19:01:43 +0100

    Removed created hook (code is in datagrid mixin)

--- src/pages/tags/results/list.vue
+++ src/pages/tags/results/list.vue
@@ -175,23 +175,6 @@ export default {
       detailRoute: 'tag-detail'
     }
   },
-  created() {
-    if (this.$route.query.property_id) {
-      this.updateParams({
-        columnFilters: { property_att: this.$route.query.property_id }
-      })
-      this.columns.find(
-        (x) => x.field === 'property_att'
-      ).filterOptions.filterValue = this.$route.query.property_id
-    }
-
-    if (this.$route.query.search) {
-      this.updateParams({
-        columnFilters: { search: this.$route.query.search }
-      })
-      this.tableFilters.search = this.$route.query.search
-    }
-  },
   methods: {
     async loadFilters() {
       await this.$axios
