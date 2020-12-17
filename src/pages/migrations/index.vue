6fa91dac18c2cb0ae3114a80915b6d12b7fa44bb
jact <jachavar@gmail.com>
2020-12-17 18:07:36 +0100

    Added migrations dashboard page

---
+++ src/pages/migrations/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header :title="title" :has-add-button="false" />
+
+    <SearchFilter @search="search" />
+
+    <div class="row">
+      <div class="col-12">
+        <StackedBarChart
+          :title="$gettext('Migrations / Month')"
+          :data="projectMonth"
+          @getLink="goTo"
+        />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <PieChart
+          :title="$gettext('Migrations / Project')"
+          :data="byProject"
+          :url="url"
+          @getLink="goTo"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import Header from 'components/ui/Header'
+import SearchFilter from 'components/ui/SearchFilter'
+import PieChart from 'components/chart/Pie'
+import StackedBarChart from 'components/chart/StackedBar'
+
+export default {
+  meta() {
+    return {
+      title: this.title
+    }
+  },
+  components: {
+    Breadcrumbs,
+    Header,
+    SearchFilter,
+    StackedBarChart,
+    PieChart
+  },
+  data() {
+    return {
+      title: this.$gettext('Migrations'),
+      breadcrumbs: [
+        {
+          text: this.$gettext('Dashboard'),
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: this.$gettext('Data'),
+          icon: 'mdi-database-search'
+        },
+        {
+          text: this.$gettext('Migrations'),
+          icon: 'mdi-map-marker-right'
+        }
+      ],
+      projectMonth: {},
+      byProject: {},
+      url: { name: 'migrations-list' }
+    }
+  },
+  async mounted() {
+    await this.$axios
+      .get('/api/v1/token/stats/migrations/project/month/')
+      .then((response) => {
+        const series = []
+
+        Object.entries(response.data.data).map(([key, val]) => {
+          series.push({
+            type: 'line',
+            smooth: true,
+            name: key,
+            data: val
+          })
+        })
+        this.projectMonth = {
+          xData: response.data.x_labels,
+          series
+        }
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
+
+    await this.$axios
+      .get('/api/v1/token/stats/migrations/project/')
+      .then((response) => {
+        this.byProject = response.data
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
+  },
+  methods: {
+    goTo(params) {
+      console.log(params)
+      if ('url' in params) {
+        let query = params.url.query || {}
+
+        if (params.data.project_id) {
+          Object.assign(query, {
+            project_id: params.data.project_id
+          })
+        }
+
+        console.log(params.url.name, query)
+        this.$router.push({ name: params.url.name, query })
+      }
+
+      if (params.data.created_at__lt) {
+        let query = {
+          created_at__gte: params.data.created_at__gte,
+          created_at__lt: params.data.created_at__lt
+        }
+
+        if (params.data.project__id__exact) {
+          Object.assign(query, { project_id: params.data.project__id__exact })
+        }
+
+        console.log(query)
+        this.$router.push(Object.assign({}, this.url, { query }))
+      }
+    },
+
+    search(value) {
+      this.$router.push(Object.assign(this.url, { query: { search: value } }))
+    }
+  }
+}
+</script>
