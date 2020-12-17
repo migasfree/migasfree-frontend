d91c64dc939af8b842c561605c5b5ce0fa8d854b
jact <jachavar@gmail.com>
2020-12-17 09:24:06 +0100

    Added attribute set detail page

---
+++ src/pages/attribute-sets/results/detail.vue
@@ -0,0 +1,174 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header :title="$gettext('Attribute Set')" :has-add-button="false">
+      <template v-if="element.id" #append
+        >:
+        <MigasLink
+          model="attribute-sets"
+          :pk="element.id"
+          :value="element.name"
+          icon="mdi-set-none"
+        />
+      </template>
+    </Header>
+
+    <q-card>
+      <q-card-section>
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-6 col-md">
+            <q-checkbox
+              v-model="element.enabled"
+              left-label
+              :label="$gettext('Enabled?')"
+            />
+          </div>
+
+          <div class="col-6 col-md">
+            <q-input
+              v-model="element.name"
+              outlined
+              :label="$gettext('Name')"
+              lazy-rules
+              :rules="[(val) => !!val || $gettext('* Required')]"
+            />
+          </div>
+        </div>
+
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-12">
+            <q-input
+              v-model="element.description"
+              outlined
+              type="textarea"
+              :label="$gettext('Description')"
+            />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions class="justify-around">
+        <q-btn
+          flat
+          color="primary"
+          :label="$gettext('Save and add other')"
+          icon="mdi-plus"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('add')"
+        />
+        <q-btn
+          flat
+          color="primary"
+          :label="$gettext('Save and continue editing')"
+          icon="mdi-content-save-edit"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement"
+        />
+        <q-btn
+          :label="$gettext('Save')"
+          color="primary"
+          icon="mdi-content-save-move"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('return')"
+        />
+      </q-card-actions>
+    </q-card>
+
+    <div v-if="$route.params.id && element.id" class="row q-pa-md">
+      <q-btn
+        flat
+        icon="mdi-delete"
+        color="negative"
+        :label="$gettext('Delete')"
+        @click="confirmRemove = true"
+      />
+    </div>
+
+    <RemoveDialog
+      v-model="confirmRemove"
+      @confirmed="remove"
+      @canceled="confirmRemove = !confirmRemove"
+    />
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import Header from 'components/ui/Header'
+import MigasLink from 'components/MigasLink'
+import RemoveDialog from 'components/ui/RemoveDialog'
+import { detailMixin } from 'mixins/detail'
+
+export default {
+  meta() {
+    return {
+      title: this.title
+    }
+  },
+  components: {
+    Breadcrumbs,
+    Header,
+    RemoveDialog,
+    MigasLink
+  },
+  mixins: [detailMixin],
+  data() {
+    const route = 'attribute-sets-list'
+    const title = this.$gettext('Attribute Set')
+    const element = { id: 0, enabled: false }
+
+    return {
+      title,
+      originalTitle: title,
+      model: 'attribute-sets',
+      listRoute: route,
+      addRoute: 'attribute-set-add',
+      detailRoute: 'attribute-set-detail',
+      breadcrumbs: [
+        {
+          text: this.$gettext('Dashboard'),
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: this.$gettext('Configuration'),
+          icon: 'mdi-cogs'
+        },
+        {
+          text: this.$gettext('Attribute Sets'),
+          icon: 'mdi-set-none',
+          to: route
+        }
+      ],
+      element,
+      emptyElement: Object.assign({}, element),
+      confirmRemove: false
+    }
+  },
+  computed: {
+    isValid() {
+      return this.element.name !== undefined && this.element.name !== ''
+    }
+  },
+  methods: {
+    elementData() {
+      return {
+        name: this.element.name,
+        description: this.element.description,
+        enabled: this.element.enabled
+      }
+    },
+
+    setRelated() {
+      /* this.element.kind = this.kind.find((x) => x.id == this.element.kind)
+      this.element.language = this.languages.find(
+        (x) => x.id === this.element.language
+      ) */
+    }
+  }
+}
+</script>
