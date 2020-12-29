1a4a5f622d57077c79c8a41eb54ab1b8700c2bdd
jact <jachavar@gmail.com>
2020-12-29 12:51:59 +0100

    Added connection detail page

---
+++ src/pages/connections/results/detail.vue
@@ -0,0 +1,194 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header :title="$gettext('Connection')" :has-add-button="false">
+      <template v-if="element.id" #append
+        >:
+        <MigasLink
+          model="devices/connections"
+          :pk="element.id"
+          :value="element.name"
+          icon="mdi-connection"
+        />
+      </template>
+    </Header>
+
+    <q-card>
+      <q-card-section>
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-6 col-md">
+            <q-input
+              v-model="element.name"
+              outlined
+              :label="$gettext('Name')"
+              lazy-rules
+              :rules="[(val) => !!val || $gettext('* Required')]"
+            />
+          </div>
+
+          <div class="col-6 col-md">
+            <q-select
+              v-model="element.device_type"
+              outlined
+              :label="$gettext('Device Type')"
+              :options="deviceTypes"
+              option-value="id"
+              option-label="name"
+              lazy-rules
+              :rules="[(val) => !!val || $gettext('* Required')]"
+            />
+          </div>
+        </div>
+
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-12">
+            <q-input
+              v-model="element.fields"
+              outlined
+              :label="$gettext('Fields')"
+              :hint="$gettext('Format: NAME[:Placeholder],...')"
+            />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions class="justify-around">
+        <q-btn
+          flat
+          color="primary"
+          :label="$gettext('Save and add other')"
+          icon="mdi-plus"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('add')"
+        />
+        <q-btn
+          flat
+          color="primary"
+          :label="$gettext('Save and continue editing')"
+          icon="mdi-content-save-edit"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement"
+        />
+        <q-btn
+          :label="$gettext('Save')"
+          color="primary"
+          icon="mdi-content-save-move"
+          :loading="loading"
+          :disabled="!isValid || loading"
+          @click="updateElement('return')"
+        />
+      </q-card-actions>
+    </q-card>
+
+    <div v-if="$route.params.id && element.id" class="row q-pa-md">
+      <q-btn
+        flat
+        icon="mdi-delete"
+        color="negative"
+        :label="$gettext('Delete')"
+        @click="confirmRemove = true"
+      />
+    </div>
+
+    <RemoveDialog
+      v-model="confirmRemove"
+      @confirmed="remove"
+      @canceled="confirmRemove = !confirmRemove"
+    />
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import Header from 'components/ui/Header'
+import MigasLink from 'components/MigasLink'
+import RemoveDialog from 'components/ui/RemoveDialog'
+import { detailMixin } from 'mixins/detail'
+
+export default {
+  meta() {
+    return {
+      title: this.title
+    }
+  },
+  components: {
+    Breadcrumbs,
+    Header,
+    RemoveDialog,
+    MigasLink
+  },
+  mixins: [detailMixin],
+  data() {
+    const route = 'connections-list'
+    const title = this.$gettext('Connection')
+    const element = { id: 0 }
+
+    return {
+      title,
+      originalTitle: title,
+      model: 'devices/connections',
+      listRoute: route,
+      addRoute: 'connection-add',
+      detailRoute: 'connection-detail',
+      breadcrumbs: [
+        {
+          text: this.$gettext('Dashboard'),
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: this.$gettext('Devices'),
+          icon: 'mdi-printer-eye'
+        },
+        {
+          text: this.$gettext('Connections'),
+          icon: 'mdi-connection',
+          to: route
+        }
+      ],
+      element,
+      emptyElement: Object.assign({}, element),
+      deviceTypes: [],
+      confirmRemove: false
+    }
+  },
+  computed: {
+    isValid() {
+      return (
+        this.element.name !== undefined &&
+        this.element.name !== '' &&
+        this.element.device_type !== undefined
+      )
+    }
+  },
+  methods: {
+    async loadRelated() {
+      await this.$axios
+        .get('/api/v1/token/devices/types/')
+        .then((response) => {
+          this.deviceTypes = response.data.results
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    },
+
+    elementData() {
+      return {
+        name: this.element.name,
+        device_type: this.element.device_type.id,
+        fields: this.element.fields
+      }
+    },
+
+    setRelated() {
+      this.element.device_type = this.deviceTypes.find(
+        (x) => x.id === this.element.device_type
+      )
+    }
+  }
+}
+</script>
