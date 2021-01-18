b957088efa28ea82fb2fb238322e5fceb0db990c
jact <jachavar@gmail.com>
2021-01-18 13:19:32 +0100

    Added devices list page

---
+++ src/pages/devices/results/list.vue
@@ -0,0 +1,291 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <Header
+      :title="title"
+      :results="totalRecords"
+      @new="$router.push({ name: 'device-add' })"
+    />
+
+    <SearchFilter
+      v-model="tableFilters.search"
+      @search="onSearch"
+      @clear="onSearchClear"
+    />
+
+    <div class="row q-pa-md">
+      <div class="col-12">
+        <q-btn :label="$gettext('Reset all filters')" @click="resetFilters" />
+      </div>
+    </div>
+
+    <vue-good-table
+      ref="myTable"
+      :columns="columns"
+      :rows="rows"
+      mode="remote"
+      compact-mode
+      :total-rows="totalRecords"
+      :is-loading.sync="isLoading"
+      :line-numbers="false"
+      :select-options="selectOptions"
+      :pagination-options="paginationOptions"
+      :search-options="searchOptions"
+      style-class="vgt-table striped condensed"
+      @on-page-change="onPageChange"
+      @on-sort-change="onSortChange"
+      @on-column-filter="onColumnFilter"
+      @on-per-page-change="onPerPageChange"
+      @on-selected-rows-change="onSelectionChanged"
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'actions'">
+          <q-btn
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-pencil"
+            color="primary"
+            @click="edit(props.row.id)"
+          />
+          <q-btn
+            class="q-ma-xs"
+            round
+            size="sm"
+            icon="mdi-delete"
+            color="negative"
+            @click="confirmRemove(props.row.id)"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'name'">
+          <MigasLink
+            model="devices/devices"
+            :pk="props.row.id"
+            :value="props.row.name"
+            icon="mdi-printer"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'model.name'">
+          <MigasLink
+            model="devices/models"
+            :pk="props.row.model.id"
+            :value="props.row.model.name"
+            icon="mdi-shape"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'connection.name'">
+          <MigasLink
+            model="devices/connections"
+            :pk="props.row.connection.id"
+            :value="props.row.connection.name"
+            icon="mdi-connection"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'model.manufacturer.name'">
+          <MigasLink
+            model="devices/manufacturers"
+            :pk="props.row.model.manufacturer.id"
+            :value="props.row.model.manufacturer.name"
+            icon="mdi-factory"
+          />
+        </span>
+        <span v-else-if="props.column.field == 'total_computers'">
+          {{ props.row.total_computers }}
+        </span>
+        <span v-else>
+          {{ props.formattedRow[props.column.field] }}
+        </span>
+      </template>
+      <div slot="emptystate" v-translate>There are no results</div>
+      <div slot="selected-row-actions">
+        <q-btn
+          size="sm"
+          color="negative"
+          icon="mdi-delete"
+          @click="confirmRemove"
+        />
+      </div>
+    </vue-good-table>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import SearchFilter from 'components/ui/SearchFilter'
+import Header from 'components/ui/Header'
+import MigasLink from 'components/MigasLink'
+import { datagridMixin } from 'mixins/datagrid'
+
+export default {
+  meta() {
+    return {
+      title: this.$gettext('Devices List')
+    }
+  },
+  components: {
+    Breadcrumbs,
+    SearchFilter,
+    Header,
+    MigasLink
+  },
+  mixins: [datagridMixin],
+  data() {
+    return {
+      title: this.$gettext('Devices'),
+      breadcrumbs: [
+        {
+          text: this.$gettext('Dashboard'),
+          to: 'home',
+          icon: 'mdi-home'
+        },
+        {
+          text: this.$gettext('Devices'),
+          icon: 'mdi-printer-eye'
+        },
+        {
+          text: this.$gettext('Devices'),
+          icon: 'mdi-printer',
+          to: 'devices-dashboard'
+        },
+        {
+          text: this.$gettext('Results')
+        }
+      ],
+      columns: [
+        {
+          field: 'id',
+          hidden: true
+        },
+        {
+          label: this.$gettext('Actions'),
+          field: 'actions',
+          html: true,
+          sortable: false,
+          globalSearchDisabled: true
+        },
+        {
+          label: this.$gettext('Name'),
+          field: 'name',
+          html: true,
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$gettext('Filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: this.$gettext('Location'),
+          field: 'location'
+        },
+        {
+          field: 'model.id',
+          hidden: true
+        },
+        {
+          label: this.$gettext('Model'),
+          field: 'model.name',
+          html: true,
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$gettext('Filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          field: 'model.manufacturer.id',
+          hidden: true
+        },
+        {
+          label: this.$gettext('Manufacturer'),
+          field: 'model.manufacturer.name',
+          html: true,
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$gettext('Filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          field: 'connection.id',
+          hidden: true
+        },
+        {
+          label: this.$gettext('Connection'),
+          field: 'connection.name',
+          html: true,
+          filterOptions: {
+            enabled: true,
+            placeholder: this.$gettext('Filter'),
+            trigger: 'enter'
+          }
+        },
+        {
+          label: this.$gettext('Computers'),
+          field: 'total_computers',
+          type: 'number'
+        }
+      ],
+      model: 'devices/devices',
+      detailRoute: 'device-detail'
+    }
+  },
+  methods: {
+    async loadFilters() {
+      await this.$axios
+        .get('/api/v1/token/devices/models/')
+        .then((response) => {
+          this.columns.find(
+            (x) => x.field === 'model.name'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      await this.$axios
+        .get('/api/v1/token/devices/manufacturers/')
+        .then((response) => {
+          this.columns.find(
+            (x) => x.field === 'model.manufacturer.name'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      await this.$axios
+        .get('/api/v1/token/devices/connections/')
+        .then((response) => {
+          this.columns.find(
+            (x) => x.field === 'connection.name'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+    }
+  }
+}
+</script>
