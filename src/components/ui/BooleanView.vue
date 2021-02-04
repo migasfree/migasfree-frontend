46e97182e11712516cf8999993e8d9b77c43708c
Jose Antonio Chavarría <jachavar@gmail.com>
2021-02-04 13:08:50 +0100

    Changed root element (div to span)

--- src/components/ui/BooleanView.vue
+++ src/components/ui/BooleanView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <span>
     <q-icon v-if="value" name="mdi-check-bold" color="positive" size="md"
       ><q-tooltip><translate>Yes</translate></q-tooltip></q-icon
     >
     <q-icon v-else name="mdi-close-thick" color="negative" size="md">
       <q-tooltip><translate>No</translate></q-tooltip>
     </q-icon>
-  </div>
+  </span>
 </template>
 
 <script>
