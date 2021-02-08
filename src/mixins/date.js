ffc1f6c27d570403efd7d793bbeb70ffd66acd5c
jact <jachavar@gmail.com>
2021-02-08 10:26:57 +0100

    Improved showDate code

--- src/mixins/date.js
+++ src/mixins/date.js
@@ -2,8 +2,14 @@ import { date } from 'quasar'
 
 export const dateMixin = {
   methods: {
-    showDate(isoString, format) {
-      format = format || 'YYYY-MM-DD HH:mm:ss'
+    showDate(isoString, format = '') {
+      if (!format) {
+        format =
+          typeof isoString === 'string' && !isoString.includes('T')
+            ? 'YYYY-MM-DD'
+            : 'YYYY-MM-DD HH:mm:ss'
+      }
+
       return date.formatDate(Date.parse(isoString), format)
     }
   }
