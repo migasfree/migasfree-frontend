2db11870b618bf0b34732f4a8025502cbac4ccc4
jact <jachavar@gmail.com>
2021-02-08 14:45:03 +0100

    Added more fields

--- src/pages/devices/results/detail.vue
+++ src/pages/devices/results/detail.vue
@@ -16,6 +16,8 @@
 
     <q-card>
       <q-card-section>
+        <div v-translate class="text-h5 q-mt-sm q-mb-xs">General</div>
+
         <div class="row q-pa-md q-gutter-md">
           <div class="col-6 col-md">
             <q-input
@@ -71,6 +73,176 @@
               </template>
             </q-select>
           </div>
+        </div>
+
+        <div class="row q-pa-md q-gutter-md">
+          <div class="col-6 col-md">
+            <q-select
+              v-model="element.model"
+              outlined
+              use-input
+              map-options
+              input-debounce="0"
+              :label="$gettext('Model')"
+              :options="models"
+              @filter="filterModels"
+              @filter-abort="abortFilterModels"
+              @input="resetConnections"
+            >
+              <template #no-option>
+                <q-item>
+                  <q-item-section v-translate class="text-grey">
+                    No results
+                  </q-item-section>
+                </q-item>
+              </template>
+
+              <template #option="scope">
+                <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+                  {{ scope.opt.name }}
+                </q-item>
+              </template>
+
+              <template #selected-item="scope">
+                <q-chip
+                  removable
+                  dense
+                  :tabindex="scope.tabindex"
+                  class="q-ma-md"
+                  @remove="scope.removeAtIndex(scope.index)"
+                >
+                  <MigasLink
+                    model="devices/models"
+                    :pk="scope.opt.id"
+                    :value="
+                      `${scope.opt.name} (${scope.opt.manufacturer.name})`
+                    "
+                    icon="mdi-shape"
+                  />
+                </q-chip>
+              </template>
+            </q-select>
+          </div>
+
+          <div class="col-6 col-md">
+            <q-select
+              v-if="element.model"
+              v-model="element.connection"
+              outlined
+              :label="$gettext('Connections')"
+              :options="element.model.connections"
+              option-value="id"
+              option-label="name"
+              lazy-rules
+              :rules="[(val) => !!val || $gettext('* Required')]"
+              @input="localConnectionFields"
+            />
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section v-if="element.connection">
+        <div v-translate class="text-h5 q-mt-sm q-mb-xs">Connection Fields</div>
+
+        <pre>{{ element.connection.fields }}</pre>
+      </q-card-section>
+
+      <q-card-section>
+        <div v-translate class="text-h5 q-mt-sm q-mb-xs">Logical Devices</div>
+
+        <q-list
+          v-if="logicalDevices.length > 0"
+          class="q-pa-md"
+          bordered
+          separator
+        >
+          <q-item v-for="(logical, index) in logicalDevices" :key="index">
+            <q-item-section side>
+              <q-btn
+                flat
+                dense
+                round
+                color="negative"
+                icon="mdi-delete"
+                @click="removeInline(index)"
+                ><q-tooltip><translate>Delete</translate></q-tooltip></q-btn
+              >
+            </q-item-section>
+
+            <q-item-section class="col-3">
+              <q-select
+                v-model="logical.capability"
+                outlined
+                :label="$gettext('Capability')"
+                :options="capabilities"
+                option-value="id"
+                option-label="name"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </q-item-section>
+
+            <q-item-section class="col-4">
+              <q-input
+                v-model="logical.alternative_capability_name"
+                outlined
+                :label="$gettext('Alternative Capability Name')"
+              />
+            </q-item-section>
+
+            <q-item-section class="col-4">
+              <q-select
+                v-model="logical.attributes"
+                outlined
+                use-input
+                map-options
+                multiple
+                input-debounce="0"
+                :label="$gettext('Attributes')"
+                :options="attributes"
+                @filter="filterAttributes"
+                @filter-abort="abortFilterAttributes"
+              >
+                <template #no-option>
+                  <q-item>
+                    <q-item-section v-translate class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+
+                <template #option="scope">
+                  <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+                    {{ attributeValue(scope.opt) }}
+                  </q-item>
+                </template>
+
+                <template #selected-item="scope">
+                  <q-chip
+                    removable
+                    dense
+                    :tabindex="scope.tabindex"
+                    class="q-ma-md"
+                    @remove="scope.removeAtIndex(scope.index)"
+                  >
+                    <MigasLink
+                      model="attributes"
+                      :pk="scope.opt.id"
+                      :value="attributeValue(scope.opt)"
+                    />
+                  </q-chip>
+                </template>
+              </q-select>
+            </q-item-section>
+          </q-item>
+        </q-list>
+
+        <div class="q-pa-md">
+          <q-btn
+            icon="mdi-plus"
+            :label="$gettext('Add other Logical Device')"
+            @click="addInline"
+          />
         </div>
       </q-card-section>
 
@@ -174,6 +346,10 @@ export default {
       element,
       emptyElement: Object.assign({}, element),
       attributes: [],
+      models: [],
+      logicalDevices: [],
+      removedLogicalDevices: [],
+      capabilities: [],
       confirmRemove: false
     }
   },
@@ -183,6 +359,34 @@ export default {
     }
   },
   methods: {
+    async loadRelated() {
+      await this.$axios
+        .get('/api/v1/token/devices/capabilities/')
+        .then((response) => {
+          this.capabilities = response.data.results
+        })
+        .catch((error) => {
+          this.$store.dispatch('ui/notifyError', error)
+        })
+
+      if (this.element.id) {
+        await this.$axios
+          .get(`/api/v1/token/devices/logical/?device__id=${this.element.id}`)
+          .then((response) => {
+            this.logicalDevices = response.data.results
+          })
+          .catch((error) => {
+            this.$store.dispatch('ui/notifyError', error)
+          })
+
+        this.localConnectionFields()
+        /* this.logicalDevices = this.element.packages_by_project
+        this.packagesByProject.forEach((item) => {
+          item.packages_to_install = item.packages_to_install.join('\n')
+        }) */
+      }
+    },
+
     elementData() {
       return {
         name: this.element.name
@@ -211,6 +415,64 @@ export default {
 
     abortFilterAttributes() {
       // console.log('delayed filter aborted')
+    },
+
+    filterModels(val, update, abort) {
+      // call abort() at any time if you can't retrieve data somehow
+      if (val.length < 3) {
+        abort()
+        return
+      }
+
+      update(() => {
+        const needle = val.toLowerCase()
+        this.$axios
+          .get('/api/v1/token/devices/models/', { params: { search: needle } })
+          .then((response) => {
+            this.models = response.data.results
+          })
+        /* this.models = stringOptions.filter(
+          (v) => v.toLowerCase().indexOf(needle) > -1
+        ) */
+      })
+    },
+
+    abortFilterModels() {
+      // console.log('delayed filter aborted')
+    },
+
+    resetConnections() {
+      this.$set(this.element, 'connection', null)
+    },
+
+    async localConnectionFields() {
+      if (this.element.connection.id)
+        await this.$axios
+          .get(
+            `/api/v1/token/devices/connections/${this.element.connection.id}/`
+          )
+          .then((response) => {
+            this.$set(this.element.connection, 'fields', response.data.fields)
+          })
+          .catch((error) => {
+            this.$store.dispatch('ui/notifyError', error)
+          })
+    },
+
+    addInline() {
+      this.logicalDevices.push({
+        id: 0,
+        capability: null,
+        alternative_capability_name: null,
+        attributes: []
+      })
+    },
+
+    removeInline(index) {
+      const removedItem = this.logicalDevices.splice(index, 1)[0]
+      if (removedItem.id > 0) {
+        this.removedLogicalDevices.push(removedItem.id)
+      }
     }
   }
 }
