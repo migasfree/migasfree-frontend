7bc0e5d0dc5e652f1b261a04340a914d3309b92d
jact <jachavar@gmail.com>
2021-02-19 13:56:56 +0100

    Added availableLanguages and currentKebabCase to configuration

--- src/boot/gettext.js
+++ src/boot/gettext.js
@@ -2,26 +2,18 @@ import Vue from 'vue'
 import GetTextPlugin from 'vue-gettext'
 import translations from '../i18n/translations.json'
 
-Vue.use(GetTextPlugin, { translations, defaultLanguage: 'es_ES' })
-
-/*
 Vue.use(GetTextPlugin, {
   availableLanguages: {
-    en_GB: 'British English',
     en_US: 'American English',
-    es_ES: 'Español',
-    fr_FR: 'Français',
-    it_IT: 'Italiano',
+    es_ES: 'Español'
   },
-  defaultLanguage: 'fr_FR',
   languageVmMixin: {
     computed: {
-      currentKebabCase: function () {
+      currentKebabCase: function() {
         return this.current.toLowerCase().replace('_', '-')
-      },
-    },
+      }
+    }
   },
   translations,
-  silent: true,
+  defaultLanguage: 'es_ES'
 })
-*/
