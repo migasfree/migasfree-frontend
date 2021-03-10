4e778a4e9e52b464129d9b59b2b481bb6d82da63
jact <jachavar@gmail.com>
2021-03-10 13:48:14 +0100

    Changed layout

--- src/pages/logical-devices/results/detail.vue
+++ src/pages/logical-devices/results/detail.vue
@@ -81,7 +81,10 @@
               option-label="name"
               lazy-rules
               :rules="[(val) => !!val || $gettext('* Required')]"
-            />
+              ><template #prepend>
+                <q-icon name="mdi-format-list-bulleted-type" />
+              </template>
+            </q-select>
           </div>
 
           <div class="col-6 col-md">
