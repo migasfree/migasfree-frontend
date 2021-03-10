46b7ece494de5646d26ed56135363ec667e51ee0
jact <jachavar@gmail.com>
2021-03-10 13:50:54 +0100

    Added isBasic computed property (if basic -> no data visible)

--- src/pages/formulas/results/detail.vue
+++ src/pages/formulas/results/detail.vue
@@ -14,164 +14,170 @@
       </template>
     </Header>
 
-    <q-card>
-      <q-card-section>
-        <div v-translate class="text-h5 q-mt-sm q-mb-xs">General</div>
+    <q-banner v-if="isBasic" rounded class="bg-warning text-black">
+      <translate>No data available.</translate>
+    </q-banner>
 
-        <div class="row q-pa-md q-gutter-md">
-          <div class="col-6 col-md">
-            <q-checkbox
-              v-model="element.enabled"
-              left-label
-              :label="$gettext('Enabled?')"
-            />
+    <template v-else>
+      <q-card>
+        <q-card-section>
+          <div v-translate class="text-h5 q-mt-sm q-mb-xs">General</div>
+
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-6 col-md">
+              <q-checkbox
+                v-model="element.enabled"
+                left-label
+                :label="$gettext('Enabled?')"
+              />
+            </div>
+
+            <div class="col-6 col-md">
+              <q-select
+                v-model="element.kind"
+                outlined
+                :label="$gettext('Kind')"
+                :options="kind"
+                option-value="id"
+                option-label="name"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </div>
           </div>
 
-          <div class="col-6 col-md">
-            <q-select
-              v-model="element.kind"
-              outlined
-              :label="$gettext('Kind')"
-              :options="kind"
-              option-value="id"
-              option-label="name"
-              lazy-rules
-              :rules="[(val) => !!val || $gettext('* Required')]"
-            />
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-6 col-md">
+              <q-input
+                v-model="element.prefix"
+                outlined
+                :label="$gettext('Prefix')"
+                counter
+                maxlength="3"
+                lazy-rules
+                :rules="[
+                  (val) => !!val || $gettext('* Required'),
+                  (val) =>
+                    val.length <= 3 ||
+                    $gettextInterpolate(
+                      $gettext('Please use maximum %{n} characters'),
+                      {
+                        n: 3
+                      }
+                    )
+                ]"
+                @input="
+                  (v) => {
+                    element.prefix = v.toUpperCase()
+                  }
+                "
+              />
+            </div>
+
+            <div class="col-6 col-md">
+              <q-input
+                v-model="element.name"
+                outlined
+                :label="$gettext('Name')"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </div>
           </div>
-        </div>
+        </q-card-section>
 
-        <div class="row q-pa-md q-gutter-md">
-          <div class="col-6 col-md">
-            <q-input
-              v-model="element.prefix"
-              outlined
-              :label="$gettext('Prefix')"
-              counter
-              maxlength="3"
-              lazy-rules
-              :rules="[
-                (val) => !!val || $gettext('* Required'),
-                (val) =>
-                  val.length <= 3 ||
-                  $gettextInterpolate(
-                    $gettext('Please use maximum %{n} characters'),
-                    {
-                      n: 3
-                    }
-                  )
-              ]"
-              @input="
-                (v) => {
-                  element.prefix = v.toUpperCase()
-                }
-              "
-            />
+        <q-card-section>
+          <div v-translate class="text-h5 q-mt-sm q-mb-xs">Code</div>
+
+          <div class="row q-pa-md q-gutter-md justify-between q-mb-xl">
+            <div class="col-md-3">
+              <q-select
+                v-model="element.language"
+                outlined
+                :label="$gettext('Language')"
+                :options="languages"
+                option-value="id"
+                option-label="name"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </div>
+
+            <div class="col-md-8 q-mb-xl">
+              <q-input
+                v-model="element.code"
+                outlined
+                type="textarea"
+                bottom-slots
+                :label="$gettext('Code')"
+              >
+                <template #hint>
+                  <p v-translate>
+                    This code will execute in the client computer, and it must
+                    put in the standard output the value of the attribute
+                    correspondent to this property.
+                  </p>
+                  <p v-translate>
+                    The format of this value is 'name~description', where
+                    'description' is optional.
+                  </p>
+                  <p><b v-translate>Example of code:</b></p>
+                  <p v-translate>
+                    # Create an attribute with the name of computer from bash
+                  </p>
+                  <p><code>echo $HOSTNAME</code></p>
+                </template></q-input
+              >
+            </div>
           </div>
+        </q-card-section>
 
-          <div class="col-6 col-md">
-            <q-input
-              v-model="element.name"
-              outlined
-              :label="$gettext('Name')"
-              lazy-rules
-              :rules="[(val) => !!val || $gettext('* Required')]"
-            />
-          </div>
-        </div>
-      </q-card-section>
+        <q-card-actions class="justify-around">
+          <q-btn
+            flat
+            color="primary"
+            :label="$gettext('Save and add other')"
+            icon="mdi-plus"
+            :loading="loading"
+            :disabled="!isValid || loading"
+            @click="updateElement('add')"
+          />
+          <q-btn
+            flat
+            color="primary"
+            :label="$gettext('Save and continue editing')"
+            icon="mdi-content-save-edit"
+            :loading="loading"
+            :disabled="!isValid || loading"
+            @click="updateElement"
+          />
+          <q-btn
+            :label="$gettext('Save')"
+            color="primary"
+            icon="mdi-content-save-move"
+            :loading="loading"
+            :disabled="!isValid || loading"
+            @click="updateElement('return')"
+          />
+        </q-card-actions>
+      </q-card>
 
-      <q-card-section>
-        <div v-translate class="text-h5 q-mt-sm q-mb-xs">Code</div>
-
-        <div class="row q-pa-md q-gutter-md justify-between q-mb-xl">
-          <div class="col-md-3">
-            <q-select
-              v-model="element.language"
-              outlined
-              :label="$gettext('Language')"
-              :options="languages"
-              option-value="id"
-              option-label="name"
-              lazy-rules
-              :rules="[(val) => !!val || $gettext('* Required')]"
-            />
-          </div>
-
-          <div class="col-md-8 q-mb-xl">
-            <q-input
-              v-model="element.code"
-              outlined
-              type="textarea"
-              bottom-slots
-              :label="$gettext('Code')"
-            >
-              <template #hint>
-                <p v-translate>
-                  This code will execute in the client computer, and it must put
-                  in the standard output the value of the attribute
-                  correspondent to this property.
-                </p>
-                <p v-translate>
-                  The format of this value is 'name~description', where
-                  'description' is optional.
-                </p>
-                <p><b v-translate>Example of code:</b></p>
-                <p v-translate>
-                  # Create an attribute with the name of computer from bash
-                </p>
-                <p><code>echo $HOSTNAME</code></p>
-              </template></q-input
-            >
-          </div>
-        </div>
-      </q-card-section>
-
-      <q-card-actions class="justify-around">
+      <div v-if="$route.params.id && element.id" class="row q-pa-md">
         <q-btn
           flat
-          color="primary"
-          :label="$gettext('Save and add other')"
-          icon="mdi-plus"
-          :loading="loading"
-          :disabled="!isValid || loading"
-          @click="updateElement('add')"
+          icon="mdi-delete"
+          color="negative"
+          :label="$gettext('Delete')"
+          @click="confirmRemove = true"
         />
-        <q-btn
-          flat
-          color="primary"
-          :label="$gettext('Save and continue editing')"
-          icon="mdi-content-save-edit"
-          :loading="loading"
-          :disabled="!isValid || loading"
-          @click="updateElement"
-        />
-        <q-btn
-          :label="$gettext('Save')"
-          color="primary"
-          icon="mdi-content-save-move"
-          :loading="loading"
-          :disabled="!isValid || loading"
-          @click="updateElement('return')"
-        />
-      </q-card-actions>
-    </q-card>
+      </div>
 
-    <div v-if="$route.params.id && element.id" class="row q-pa-md">
-      <q-btn
-        flat
-        icon="mdi-delete"
-        color="negative"
-        :label="$gettext('Delete')"
-        @click="confirmRemove = true"
+      <RemoveDialog
+        v-model="confirmRemove"
+        @confirmed="remove"
+        @canceled="confirmRemove = !confirmRemove"
       />
-    </div>
-
-    <RemoveDialog
-      v-model="confirmRemove"
-      @confirmed="remove"
-      @canceled="confirmRemove = !confirmRemove"
-    />
+    </template>
   </q-page>
 </template>
 
@@ -241,6 +247,10 @@ export default {
         this.element.prefix.length <= 3 &&
         this.element.code !== undefined
       )
+    },
+
+    isBasic() {
+      return this.element.id && this.element.sort === 'basic'
     }
   },
   methods: {
