a71c002526cbdfb9586a66b29a2270c1e3599fb5
jact <jachavar@gmail.com>
2021-03-12 09:51:06 +0100

    Added vue2-leaflet (and leaflet) component

---
+++ src/boot/leaflet.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+Vue.component('LMap', LMap)
+Vue.component('LTileLayer', LTileLayer)
+Vue.component('LMarker', LMarker)
+Vue.component('LTooltip', LTooltip)
