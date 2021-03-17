4d9bc23fb4ae75443e4203b7a00e94946bde3bf6
jact <jachavar@gmail.com>
2021-03-17 10:17:54 +0100

    Added LControl component

--- src/boot/leaflet.js
+++ src/boot/leaflet.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet'
+import { LMap, LTileLayer, LMarker, LTooltip, LControl } from 'vue2-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 Vue.component('LMap', LMap)
 Vue.component('LTileLayer', LTileLayer)
 Vue.component('LMarker', LMarker)
 Vue.component('LTooltip', LTooltip)
+Vue.component('LControl', LControl)
