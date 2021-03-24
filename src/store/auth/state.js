1e1a847970a9184a4630cefc54796e19a755459f
jact <jachavar@gmail.com>
2021-03-24 18:40:20 +0100

    Added domains and scopes to state

--- src/store/auth/state.js
+++ src/store/auth/state.js
@@ -1,9 +1,12 @@
 import { LocalStorage } from 'quasar'
+import { translate } from 'vue-gettext'
 
 export default function() {
   return {
     token: LocalStorage.getItem('auth.token') || '',
     loggedIn: LocalStorage.getItem('auth.loggedIn') || false,
-    user: LocalStorage.getItem('auth.user') || {}
+    user: LocalStorage.getItem('auth.user') || {},
+    domains: [{ id: 0, name: translate.gettext('All').toUpperCase() }],
+    scopes: [{ id: 0, name: translate.gettext('All').toLowerCase() }]
   }
 }
