2b390da25c0115fd4dcafed200f3789970c20e13
Jose Antonio Chavarría <jachavar@gmail.com>
2021-03-25 11:29:12 +0100

    Use of LocalStorage for domains and scopes

--- src/store/auth/state.js
+++ src/store/auth/state.js
@@ -6,7 +6,11 @@ export default function() {
     token: LocalStorage.getItem('auth.token') || '',
     loggedIn: LocalStorage.getItem('auth.loggedIn') || false,
     user: LocalStorage.getItem('auth.user') || {},
-    domains: [{ id: 0, name: translate.gettext('All').toUpperCase() }],
-    scopes: [{ id: 0, name: translate.gettext('All').toLowerCase() }]
+    domains: LocalStorage.getItem('auth.domains') || [
+      { id: 0, name: translate.gettext('All').toUpperCase() }
+    ],
+    scopes: LocalStorage.getItem('auth.scopes') || [
+      { id: 0, name: translate.gettext('All').toLowerCase() }
+    ]
   }
 }
