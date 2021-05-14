d52a4f12027be5ac7171a73af548d4825534e9ec
jact <jachavar@gmail.com>
2021-05-14 16:56:40 +0200

    Added Models by Project stats

--- src/pages/models/index.vue
+++ src/pages/models/index.vue
@@ -16,6 +16,16 @@
         />
       </div>
     </div>
+
+    <div class="row">
+      <div class="col">
+        <StackedBarChart
+          :title="$gettext('Models / Project')"
+          :initial-data="byProject"
+          @getLink="goTo"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -24,6 +34,7 @@ import Breadcrumbs from 'components/ui/Breadcrumbs'
 import Header from 'components/ui/Header'
 import SearchFilter from 'components/ui/SearchFilter'
 import PieChart from 'components/chart/Pie'
+import StackedBarChart from 'components/chart/StackedBar'
 
 export default {
   meta() {
@@ -35,7 +46,8 @@ export default {
     Breadcrumbs,
     SearchFilter,
     Header,
-    PieChart
+    PieChart,
+    StackedBarChart
   },
   data() {
     return {
@@ -55,8 +67,33 @@ export default {
           icon: 'mdi-shape'
         }
       ],
-      url: { name: 'models-list' }
+      url: { name: 'models-list' },
+      byProject: {}
     }
+  },
+  async mounted() {
+    await this.$axios
+      .get('/api/v1/token/stats/devices/models/project/')
+      .then((response) => {
+        this.$set(this.byProject, 'xData', response.data.x_labels)
+        this.$set(this.byProject, 'series', {
+          type: 'bar',
+          data: response.data.data,
+          name: this.$gettext('Models'),
+          markLine: {
+            data: [
+              {
+                label: { show: true },
+                name: this.$gettext('Total'),
+                yAxis: response.data.total
+              }
+            ]
+          }
+        })
+      })
+      .catch((error) => {
+        this.$store.dispatch('ui/notifyError', error)
+      })
   },
   methods: {
     goTo(params) {
@@ -64,6 +101,14 @@ export default {
         this.$router.push(
           Object.assign(this.url, {
             query: { manufacturer_id: params.data.manufacturer_id }
+          })
+        )
+      }
+
+      if (params.data.drivers__project__id) {
+        this.$router.push(
+          Object.assign(this.url, {
+            query: { drivers_project_id: params.data.drivers__project__id }
           })
         )
       }
