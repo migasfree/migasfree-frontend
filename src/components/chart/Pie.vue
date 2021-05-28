6e5a59cf9828e237c574c368bb73567eccaf30fe
jact <jachavar@gmail.com>
2021-05-28 14:08:45 +0200

    Added saveImage and dataView methods (instead echarts toolbox)

--- src/components/chart/Pie.vue
+++ src/components/chart/Pie.vue
@@ -1,63 +1,109 @@
 <template>
-  <q-card class="q-ma-sm">
-    <q-card-section>
-      <div class="row">
-        <div class="col">
-          <div class="text-h5">{{ title }}</div>
-        </div>
+  <div>
+    <q-card class="q-ma-sm">
+      <q-card-section>
+        <div class="row">
+          <div class="col">
+            <div class="text-h5">{{ title }}</div>
+          </div>
 
-        <div class="col-auto">
-          <q-chip
-            size="md"
-            :clickable="data.total > 0"
-            :color="critical ? 'negative' : 'info'"
-            :text-color="critical ? 'white' : 'black'"
-            @click="goTo"
-            >{{ data.total }}</q-chip
+          <div class="col-auto">
+            <q-chip
+              size="md"
+              :clickable="data.total > 0"
+              :color="critical ? 'negative' : 'info'"
+              :text-color="critical ? 'white' : 'black'"
+              @click="goTo"
+              >{{ data.total }}</q-chip
+            >
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-section class="echart-container">
+        <v-chart
+          v-show="isChartVisible"
+          ref="chart"
+          :init-options="initOptions"
+          :option="options"
+          :loading="loading"
+          :loading-options="loadingOptions"
+          autoresize
+          @click="passData"
+        />
+        <q-banner v-if="noData" rounded class="bg-warning text-black">
+          <translate>No data available.</translate>
+        </q-banner>
+      </q-card-section>
+
+      <q-card-actions align="around">
+        <q-btn icon="mdi-database-search" flat @click="dataView">
+          <q-tooltip>{{ $gettext('Data View') }}</q-tooltip>
+        </q-btn>
+
+        <q-btn icon="mdi-download" flat @click="saveImage">
+          <q-tooltip>{{ $gettext('Save as Image') }}</q-tooltip>
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-dialog v-model="viewData">
+      <q-card>
+        <q-card-section>
+          <div class="text-h5">
+            <q-icon
+              name="mdi-database-search"
+              size="lg"
+              class="vertical-middle"
+            />
+            <translate class="vertical-middle">Data</translate>
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-table
+            v-for="(serie, index) in options.series"
+            :key="index"
+            class="q-ma-md"
+            :data="serie.data"
+            :columns="columns"
+            :pagination="{ rowsPerPage: 0 }"
+            row-key="name"
+            hide-header
+            hide-pagination
+          />
+          <q-banner
+            v-if="options.series.length === 0"
+            class="text-white bg-info q-ma-md"
           >
-        </div>
-      </div>
-    </q-card-section>
+            <translate>No information</translate>
+          </q-banner>
+        </q-card-section>
 
-    <q-card-section class="echart-container">
-      <v-chart
-        v-show="isChartVisible"
-        ref="chart"
-        :init-options="initOptions"
-        :option="options"
-        :loading="loading"
-        :loading-options="loadingOptions"
-        autoresize
-        @click="passData"
-      />
-      <q-banner v-if="noData" rounded class="bg-warning text-black">
-        <translate>No data available.</translate>
-      </q-banner>
-    </q-card-section>
-  </q-card>
+        <q-card-actions align="right">
+          <q-btn
+            v-close-popup
+            flat
+            :label="$gettext('Close')"
+            color="primary"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
 import * as echarts from 'echarts/core'
 import { PieChart } from 'echarts/charts'
-import {
-  TooltipComponent,
-  ToolboxComponent,
-  TitleComponent
-} from 'echarts/components'
+import { TooltipComponent, TitleComponent } from 'echarts/components'
 import { SVGRenderer } from 'echarts/renderers'
 import {
   MIGASFREE_CHART_COLORS,
   MIGASFREE_CHART_DARK_COLORS
 } from 'config/app.conf'
 
-echarts.use([
-  PieChart,
-  TooltipComponent,
-  ToolboxComponent,
-  TitleComponent,
-  SVGRenderer
-])
+echarts.use([PieChart, TooltipComponent, TitleComponent, SVGRenderer])
 
 export default {
   name: 'PieChart',
@@ -99,26 +145,6 @@ export default {
         title: {
           text: this.title,
           show: false
-        },
-        toolbox: {
-          show: true,
-          itemSize: 20,
-          iconStyle: {
-            color: this.$q.dark.isActive ? 'white' : 'black'
-          },
-          feature: {
-            dataView: {
-              readOnly: true,
-              title: this.$gettext('Data View'),
-              icon:
-                'path://M18.68,12.32C16.92,10.56 14.07,10.57 12.32,12.33C10.56,14.09 10.56,16.94 12.32,18.69C13.81,20.17 16.11,20.43 17.89,19.32L21,22.39L22.39,21L19.3,17.89C20.43,16.12 20.17,13.8 18.68,12.32M17.27,17.27C16.29,18.25 14.71,18.24 13.73,17.27C12.76,16.29 12.76,14.71 13.74,13.73C14.71,12.76 16.29,12.76 17.27,13.73C18.24,14.71 18.24,16.29 17.27,17.27M10.9,20.1C10.25,19.44 9.74,18.65 9.42,17.78C6.27,17.25 4,15.76 4,14V17C4,19.21 7.58,21 12,21V21C11.6,20.74 11.23,20.44 10.9,20.1M4,9V12C4,13.68 6.07,15.12 9,15.7C9,15.63 9,15.57 9,15.5C9,14.57 9.2,13.65 9.58,12.81C6.34,12.3 4,10.79 4,9M12,3C7.58,3 4,4.79 4,7C4,9 7,10.68 10.85,11H10.9C12.1,9.74 13.76,9 15.5,9C16.41,9 17.31,9.19 18.14,9.56C19.17,9.09 19.87,8.12 20,7C20,4.79 16.42,3 12,3Z'
-            },
-            saveAsImage: {
-              name: this.title,
-              title: this.$gettext('Save as Image'),
-              icon: 'path://M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z'
-            }
-          }
         }
       },
       normalSeries: [
@@ -176,7 +202,21 @@ export default {
           ? 'rgba(255, 255, 255, 0.5)'
           : 'rgba(0, 0, 0, 0.5)',
         maskColor: this.$q.dark.isActive ? '#3A4149' : 'white'
-      }
+      },
+      viewData: false,
+      columns: [
+        {
+          name: 'name',
+          field: 'name',
+          label: this.$gettext('Name'),
+          align: 'left'
+        },
+        {
+          name: 'value',
+          field: 'value',
+          label: this.$gettext('Value')
+        }
+      ]
     }
   },
   computed: {
@@ -205,8 +245,6 @@ export default {
       this.options.color = val
         ? MIGASFREE_CHART_DARK_COLORS
         : MIGASFREE_CHART_COLORS
-
-      this.options.toolbox.iconStyle.color = val ? 'white' : 'black'
 
       if ('series' in this.options) {
         this.options.series.map((item) => {
@@ -244,15 +282,32 @@ export default {
     passData(params) {
       this.$emit('getLink', Object.assign(params, { url: this.url }))
     },
+
     windowResize() {
       if (this.$refs.chart !== null && this.$refs.chart !== undefined) {
         this.$refs.chart.resize()
       }
     },
+
     goTo() {
       if (this.url) {
         this.$router.push(this.url)
       }
+    },
+
+    saveImage() {
+      const linkSource = this.$refs.chart.getDataURL()
+      const downloadLink = document.createElement('a')
+
+      document.body.appendChild(downloadLink)
+      downloadLink.href = linkSource
+      downloadLink.target = '_self'
+      downloadLink.download = `${this.title}.svg`
+      downloadLink.click()
+    },
+
+    dataView() {
+      this.viewData = true
     }
   }
 }
--- src/components/chart/StackedBar.vue
+++ src/components/chart/StackedBar.vue
@@ -1,25 +1,85 @@
 <template>
-  <q-card class="q-ma-sm">
-    <q-card-section>
-      <div class="text-h5">{{ title }}</div>
-    </q-card-section>
+  <div>
+    <q-card class="q-ma-sm">
+      <q-card-section>
+        <div class="text-h5">{{ title }}</div>
+      </q-card-section>
 
-    <q-card-section class="echart-container">
-      <v-chart
-        v-show="isChartVisible"
-        ref="chart"
-        :init-options="initOptions"
-        :option="options"
-        :loading="loading"
-        :loading-options="loadingOptions"
-        autoresize
-        @click="passData"
-      />
-      <q-banner v-if="noData" rounded class="bg-warning text-black">
-        <translate>No data available.</translate>
-      </q-banner>
-    </q-card-section>
-  </q-card>
+      <q-card-section class="echart-container">
+        <v-chart
+          v-show="isChartVisible"
+          ref="chart"
+          :init-options="initOptions"
+          :option="options"
+          :loading="loading"
+          :loading-options="loadingOptions"
+          autoresize
+          @click="passData"
+        />
+        <q-banner v-if="noData" rounded class="bg-warning text-black">
+          <translate>No data available.</translate>
+        </q-banner>
+      </q-card-section>
+
+      <q-card-actions align="around">
+        <q-btn icon="mdi-database-search" flat @click="dataView">
+          <q-tooltip>{{ $gettext('Data View') }}</q-tooltip>
+        </q-btn>
+
+        <q-btn icon="mdi-download" flat @click="saveImage">
+          <q-tooltip>{{ $gettext('Save as Image') }}</q-tooltip>
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-dialog v-model="viewData">
+      <q-card>
+        <q-card-section>
+          <div class="text-h5">
+            <q-icon
+              name="mdi-database-search"
+              size="lg"
+              class="vertical-middle"
+            />
+            <translate class="vertical-middle">Data</translate>
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-table
+            v-if="isChartVisible"
+            class="q-ma-md"
+            :pagination="{ rowsPerPage: 0 }"
+            :data="data.series"
+            hide-pagination
+          >
+            <q-tr slot="header">
+              <q-th></q-th>
+              <q-th v-for="(item, index) in data.xData" :key="index">{{
+                item
+              }}</q-th>
+            </q-tr>
+
+            <q-tr slot="body" slot-scope="props" :props="props">
+              <q-th>{{ props.row.name }}</q-th>
+              <q-td v-for="(item, index) in props.row.data" :key="index">
+                {{ item.value }}
+              </q-td>
+            </q-tr>
+          </q-table>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn
+            v-close-popup
+            flat
+            :label="$gettext('Close')"
+            color="primary"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
@@ -29,7 +89,6 @@ import { BarChart } from 'echarts/charts'
 import {
   GridComponent,
   TooltipComponent,
-  ToolboxComponent,
   LegendComponent,
   MarkPointComponent,
   MarkLineComponent
@@ -45,7 +104,6 @@ echarts.use([
   BarChart,
   GridComponent,
   TooltipComponent,
-  ToolboxComponent,
   LegendComponent,
   MarkPointComponent,
   MarkLineComponent,
@@ -109,26 +167,6 @@ export default {
         title: {
           text: this.title,
           show: false
-        },
-        toolbox: {
-          show: true,
-          itemSize: 20,
-          iconStyle: {
-            color: this.$q.dark.isActive ? 'white' : 'black'
-          },
-          feature: {
-            dataView: {
-              readOnly: true,
-              title: this.$gettext('Data View'),
-              icon:
-                'path://M18.68,12.32C16.92,10.56 14.07,10.57 12.32,12.33C10.56,14.09 10.56,16.94 12.32,18.69C13.81,20.17 16.11,20.43 17.89,19.32L21,22.39L22.39,21L19.3,17.89C20.43,16.12 20.17,13.8 18.68,12.32M17.27,17.27C16.29,18.25 14.71,18.24 13.73,17.27C12.76,16.29 12.76,14.71 13.74,13.73C14.71,12.76 16.29,12.76 17.27,13.73C18.24,14.71 18.24,16.29 17.27,17.27M10.9,20.1C10.25,19.44 9.74,18.65 9.42,17.78C6.27,17.25 4,15.76 4,14V17C4,19.21 7.58,21 12,21V21C11.6,20.74 11.23,20.44 10.9,20.1M4,9V12C4,13.68 6.07,15.12 9,15.7C9,15.63 9,15.57 9,15.5C9,14.57 9.2,13.65 9.58,12.81C6.34,12.3 4,10.79 4,9M12,3C7.58,3 4,4.79 4,7C4,9 7,10.68 10.85,11H10.9C12.1,9.74 13.76,9 15.5,9C16.41,9 17.31,9.19 18.14,9.56C19.17,9.09 19.87,8.12 20,7C20,4.79 16.42,3 12,3Z'
-            },
-            saveAsImage: {
-              name: this.title,
-              title: this.$gettext('Save as Image'),
-              icon: 'path://M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z'
-            }
-          }
         }
       },
       initOptions: {
@@ -143,7 +181,8 @@ export default {
           ? 'rgba(255, 255, 255, 0.5)'
           : 'rgba(0, 0, 0, 0.5)',
         maskColor: this.$q.dark.isActive ? '#3A4149' : 'white'
-      }
+      },
+      viewData: false
     }
   },
   computed: {
@@ -183,7 +222,6 @@ export default {
       this.options.legend.textStyle.color = val ? '#fff' : '#333'
       this.options.xAxis.axisLine.lineStyle.color = val ? '#fff' : '#333'
       this.options.yAxis.axisLine.lineStyle.color = val ? '#fff' : '#333'
-      this.options.toolbox.iconStyle.color = val ? 'white' : 'black'
     }
   },
   async mounted() {
@@ -225,10 +263,26 @@ export default {
     passData(params) {
       this.$emit('getLink', params)
     },
+
     windowResize() {
       if (this.$refs.chart !== null && this.$refs.chart !== undefined) {
         this.$refs.chart.resize()
       }
+    },
+
+    saveImage() {
+      const linkSource = this.$refs.chart.getDataURL()
+      const downloadLink = document.createElement('a')
+
+      document.body.appendChild(downloadLink)
+      downloadLink.href = linkSource
+      downloadLink.target = '_self'
+      downloadLink.download = `${this.title}.svg`
+      downloadLink.click()
+    },
+
+    dataView() {
+      this.viewData = true
     }
   }
 }
