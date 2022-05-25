3de1d91182988a0eb4cf750bfb5ec899d2915d68
Jose Antonio Chavarría <jachavar@gmail.com>
2022-05-25 12:19:55 +0200

    Added localeDate data to translate calendar

--- src/mixins/date.js
+++ src/mixins/date.js
@@ -5,6 +5,62 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
 export const dateMixin = {
+  data() {
+    return {
+      localeDate: {
+        days: [
+          this.$gettext('Sunday'),
+          this.$gettext('Monday'),
+          this.$gettext('Tuesday'),
+          this.$gettext('Wednesday'),
+          this.$gettext('Thursday'),
+          this.$gettext('Friday'),
+          this.$gettext('Saturday'),
+        ],
+        daysShort: [
+          this.$gettext('Sun'),
+          this.$gettext('Mon'),
+          this.$gettext('Tue'),
+          this.$gettext('Wed'),
+          this.$gettext('Thu'),
+          this.$gettext('Fri'),
+          this.$gettext('Sat'),
+        ],
+        months: [
+          this.$gettext('January'),
+          this.$gettext('February'),
+          this.$gettext('March'),
+          this.$gettext('April'),
+          this.$gettext('May'),
+          this.$gettext('June'),
+          this.$gettext('July'),
+          this.$gettext('August'),
+          this.$gettext('September'),
+          this.$gettext('October'),
+          this.$gettext('November'),
+          this.$gettext('December'),
+        ],
+        monthsShort: [
+          this.$gettext('Jan'),
+          this.$gettext('Feb'),
+          this.$gettext('Mar'),
+          this.$gettext('Apr'),
+          this.$gettext('May'),
+          this.$gettext('Jun'),
+          this.$gettext('Jul'),
+          this.$gettext('Aug'),
+          this.$gettext('Sep'),
+          this.$gettext('Oct'),
+          this.$gettext('Nov'),
+          this.$gettext('Dec'),
+        ],
+        firstDayOfWeek: this.$language.current.startsWith('es') ? 1 : 0,
+        format24h: true,
+        pluralDay: this.$gettext('days'),
+      },
+    }
+  },
+
   methods: {
     showDate(isoString, format = '') {
       if (!format) {
@@ -25,6 +81,6 @@ export const dateMixin = {
         })
 
       return dayjs(isoString).fromNow()
-    }
-  }
+    },
+  },
 }
