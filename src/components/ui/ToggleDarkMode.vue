644330b20e3ff83521a55451329989ce051e6fc3
Jose Antonio Chavarría <jachavar@gmail.com>
2022-05-26 12:42:34 +0200

    Added ToggleDarkMode component

---
+++ src/components/ui/ToggleDarkMode.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-btn
+    flat
+    round
+    :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"
+    @click="toggleDarkMode"
+  >
+    <q-tooltip>{{
+      $q.dark.isActive
+        ? $gettext('Switch to Light mode')
+        : $gettext('Switch to Dark mode')
+    }}</q-tooltip>
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: 'ToggleDarkMode',
+  methods: {
+    toggleDarkMode() {
+      this.$q.dark.toggle()
+      this.$q.cookies.set('darkMode', this.$q.dark.isActive, { expires: '30d' })
+    },
+  },
+}
+</script>
