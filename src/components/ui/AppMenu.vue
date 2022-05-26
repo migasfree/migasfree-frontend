64c4fc89186813b0ca2bead8b7ddf499a3f52828
Jose Antonio Chavarría <jachavar@gmail.com>
2022-05-26 10:35:42 +0200

    Changed domain icon

--- src/components/ui/AppMenu.vue
+++ src/components/ui/AppMenu.vue
@@ -32,18 +32,18 @@ export default {
           title: this.$gettext('Connections'),
           to: 'connections-list',
           icon: 'mdi-connection',
-          separatorBefore: true
+          separatorBefore: true,
         },
         {
           title: this.$gettext('Device Types'),
           to: 'device-types-list',
-          icon: 'mdi-devices'
+          icon: 'mdi-devices',
         },
         {
           title: this.$gettext('Logical Devices'),
           to: 'logical-devices-list',
-          icon: 'mdi-printer-settings'
-        }
+          icon: 'mdi-printer-settings',
+        },
       ]
 
     return {
@@ -55,56 +55,56 @@ export default {
             {
               title: this.$gettext('Platforms'),
               to: 'platforms-list',
-              icon: 'mdi-layers'
+              icon: 'mdi-layers',
             },
             {
               title: this.$gettext('Projects'),
               to: 'projects-list',
               icon: 'mdi-sitemap',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Formulas'),
               to: 'formulas-list',
-              icon: 'mdi-function-variant'
+              icon: 'mdi-function-variant',
             },
             {
               title: this.$gettext('Stamps'),
               to: 'stamps-list',
-              icon: 'mdi-stamper'
+              icon: 'mdi-stamper',
             },
             {
               title: this.$gettext('Attribute Sets'),
               to: 'attribute-sets-list',
-              icon: 'mdi-set-none'
+              icon: 'mdi-set-none',
             },
             {
               title: this.$gettext('Fault Definitions'),
               to: 'fault-definitions-list',
               icon: 'mdi-alert-octagram-outline',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('User Profiles'),
               to: 'user-profiles-list',
-              icon: 'mdi-account-cog'
+              icon: 'mdi-account-cog',
             },
             {
               title: this.$gettext('Groups'),
               to: 'groups-list',
-              icon: 'mdi-account-group'
+              icon: 'mdi-account-group',
             },
             {
               title: this.$gettext('Domains'),
               to: 'domains-list',
-              icon: 'mdi-earth'
+              icon: 'mdi-web',
             },
             {
               title: this.$gettext('Scopes'),
               to: 'scopes-list',
-              icon: 'mdi-eye-outline'
-            }
-          ]
+              icon: 'mdi-eye-outline',
+            },
+          ],
         },
         {
           icon: 'mdi-printer-eye',
@@ -113,30 +113,30 @@ export default {
             {
               title: this.$gettext('Manufacturers'),
               to: 'manufacturers-list',
-              icon: 'mdi-factory'
+              icon: 'mdi-factory',
             },
             {
               title: this.$gettext('Models'),
               to: 'models-dashboard',
-              icon: 'mdi-shape'
+              icon: 'mdi-shape',
             },
             {
               title: this.$gettext('Capabilities'),
               to: 'capabilities-list',
-              icon: 'mdi-format-list-bulleted-type'
+              icon: 'mdi-format-list-bulleted-type',
             },
             {
               title: this.$gettext('Devices'),
               to: 'devices-dashboard',
               icon: 'mdi-printer',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Devices Replacement'),
               to: 'devices-replacement',
-              icon: 'mdi-compare-horizontal'
-            }
-          ].concat(extraDevices)
+              icon: 'mdi-compare-horizontal',
+            },
+          ].concat(extraDevices),
         },
         {
           icon: 'mdi-truck-delivery',
@@ -145,46 +145,46 @@ export default {
             {
               title: this.$gettext('Deployments'),
               to: 'deployments-dashboard',
-              icon: 'mdi-rocket-launch'
+              icon: 'mdi-rocket-launch',
             },
             {
               title: this.$gettext('Schedules'),
               to: 'schedules-list',
               icon: 'mdi-calendar-start',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Stores'),
               to: 'stores-dashboard',
-              icon: 'mdi-store-24-hour'
+              icon: 'mdi-store-24-hour',
             },
             {
               title: this.$gettext('Packages'),
               to: 'packages-dashboard',
-              icon: 'mdi-package-variant'
+              icon: 'mdi-package-variant',
             },
             {
               title: this.$gettext('Package Sets'),
               to: 'package-sets-list',
               icon: 'mdi-apps-box',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Applications'),
               to: 'apps-dashboard',
-              icon: 'mdi-apps'
+              icon: 'mdi-apps',
             },
             {
               title: this.$gettext('Application Categories'),
               to: 'categories-list',
-              icon: 'mdi-format-list-bulleted-type'
+              icon: 'mdi-format-list-bulleted-type',
             },
             {
               title: this.$gettext('Policies'),
               to: 'policies-list',
-              icon: 'mdi-shield-half-full'
-            }
-          ]
+              icon: 'mdi-shield-half-full',
+            },
+          ],
         },
         {
           icon: 'mdi-database-search',
@@ -193,80 +193,80 @@ export default {
             {
               title: this.$gettext('Computers'),
               to: 'computers-dashboard',
-              icon: 'mdi-desktop-classic'
+              icon: 'mdi-desktop-classic',
             },
             {
               title: this.$gettext('Computers Replacement'),
               to: 'computers-replacement',
-              icon: 'mdi-compare-horizontal'
+              icon: 'mdi-compare-horizontal',
             },
             {
               title: this.$gettext('Software Compare'),
               to: 'computers-software-compare',
-              icon: 'mdi-file-compare'
+              icon: 'mdi-file-compare',
             },
             {
               title: this.$gettext('Packages History'),
               to: 'packages-history-dashboard',
-              icon: 'mdi-history'
+              icon: 'mdi-history',
             },
             {
               title: this.$gettext('Users'),
               to: 'users-list',
               icon: 'mdi-account',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Attributes'),
               to: 'attributes-dashboard',
-              icon: 'mdi-pound'
+              icon: 'mdi-pound',
             },
             {
               title: this.$gettext('Tags'),
               to: 'tags-dashboard',
               icon: 'mdi-tag',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Synchronizations'),
               to: 'syncs-dashboard',
-              icon: 'mdi-sync'
+              icon: 'mdi-sync',
             },
             {
               title: this.$gettext('Errors'),
               to: 'errors-dashboard',
-              icon: 'mdi-bug'
+              icon: 'mdi-bug',
             },
             {
               title: this.$gettext('Faults'),
               to: 'faults-dashboard',
-              icon: 'mdi-bomb'
+              icon: 'mdi-bomb',
             },
             {
               title: this.$gettext('Status Logs'),
               to: 'status-logs-dashboard',
-              icon: 'mdi-flag-variant'
+              icon: 'mdi-flag-variant',
             },
             {
               title: this.$gettext('Migrations'),
               to: 'migrations-dashboard',
               icon: 'mdi-map-marker-right',
-              separatorAfter: true
+              separatorAfter: true,
             },
             {
               title: this.$gettext('Messages'),
               to: 'messages-list',
-              icon: 'mdi-message-text'
+              icon: 'mdi-message-text',
             },
             {
               title: this.$gettext('Notifications'),
               to: 'notifications-dashboard',
-              icon: 'mdi-android-messages'
-            }
-          ]
-        }
-      ]
+              icon: 'mdi-android-messages',
+            },
+          ],
+        },
+      ],
     }
-  }
+  },
 }
 </script>
