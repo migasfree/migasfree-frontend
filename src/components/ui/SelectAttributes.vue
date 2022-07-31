1e9dc96c52577d90447d48baefcd6f794848edf0
jact <jachavar@gmail.com>
2022-07-31 12:05:06 +0200

    Upgraded code to Vue3 and Quasar2 (#1)

--- src/components/ui/SelectAttributes.vue
+++ src/components/ui/SelectAttributes.vue
@@ -1,85 +1,85 @@
 <template>
-  <div>
-    <q-select
-      ref="attributes"
-      v-model="localValue"
-      outlined
-      use-input
-      map-options
-      multiple
-      counter
-      input-debounce="0"
-      clearable
-      :label="label"
-      :options="attributes"
-      @filter="filterAttributes"
-      @filter-abort="abortFilterAttributes"
-      @input="updateAttributes"
+  <q-select
+    ref="attributesComponent"
+    v-model="localValue"
+    outlined
+    use-input
+    map-options
+    multiple
+    counter
+    input-debounce="0"
+    clearable
+    :label="label"
+    :options="attributes"
+    @filter="filterAttributes"
+    @filter-abort="abortFilterAttributes"
+  >
+    <template #no-option>
+      <q-item>
+        <q-item-section v-translate class="text-grey">
+          No results
+        </q-item-section>
+      </q-item>
+    </template>
+
+    <template #option="scope">
+      <q-item v-bind="scope.itemProps">
+        {{ attributeValue(scope.opt) }}
+      </q-item>
+    </template>
+
+    <template #selected-item="scope">
+      <q-chip
+        removable
+        dense
+        :tabindex="scope.tabindex"
+        class="q-ma-md"
+        @remove="scope.removeAtIndex(scope.index)"
+      >
+        <MigasLink
+          :model="equivalentModel(scope.opt)"
+          :pk="equivalentKey(scope.opt)"
+          :value="attributeValue(scope.opt)"
+          :icon="elementIcon(scope.opt.status)"
+          :tooltip="scope.opt.summary"
+        />
+      </q-chip>
+    </template>
+  </q-select>
+
+  <div v-if="hasPaste" class="row">
+    <q-btn
+      flat
+      icon="mdi-content-copy"
+      color="primary"
+      @click.stop="copyList(model, localValue)"
+      ><q-tooltip>{{ $gettext('Copy') }}</q-tooltip></q-btn
     >
-      <template #no-option>
-        <q-item>
-          <q-item-section v-translate class="text-grey">
-            No results
-          </q-item-section>
-        </q-item>
-      </template>
 
-      <template #option="scope">
-        <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
-          {{ attributeValue(scope.opt) }}
-        </q-item>
-      </template>
-
-      <template #selected-item="scope">
-        <q-chip
-          removable
-          dense
-          :tabindex="scope.tabindex"
-          class="q-ma-md"
-          @remove="scope.removeAtIndex(scope.index)"
-        >
-          <MigasLink
-            :model="equivalentModel(scope.opt)"
-            :pk="equivalentKey(scope.opt)"
-            :value="attributeValue(scope.opt)"
-            :icon="elementIcon(scope.opt.status)"
-            :tooltip="scope.opt.summary"
-          />
-        </q-chip>
-      </template>
-    </q-select>
-
-    <div v-if="hasPaste" class="row">
-      <q-btn
-        flat
-        icon="mdi-content-copy"
-        color="primary"
-        @click.stop="copyList(model, 'attributes')"
-        ><q-tooltip>{{ $gettext('Copy') }}</q-tooltip></q-btn
-      >
-
-      <q-btn
-        flat
-        icon="mdi-content-paste"
-        color="primary"
-        @click.stop="pasteList('attributes')"
-        ><q-tooltip>{{ $gettext('Paste') }}</q-tooltip></q-btn
-      >
-    </div>
+    <q-btn
+      flat
+      icon="mdi-content-paste"
+      color="primary"
+      @click.stop="pasteList(attributesComponent)"
+      ><q-tooltip>{{ $gettext('Paste') }}</q-tooltip></q-btn
+    >
   </div>
 </template>
 
 <script>
+import { ref, reactive, onMounted, watch } from 'vue'
+
+import { api } from 'boot/axios'
 import MigasLink from 'components/MigasLink'
-import { elementMixin } from 'mixins/element'
-import { copyPasteMixin } from 'mixins/copyPaste'
+
+import useCopyPaste from 'composables/copyPaste'
+import { useElement } from 'composables/element'
 
 export default {
   name: 'SelectAttributes',
   components: { MigasLink },
-  mixins: [elementMixin, copyPasteMixin],
   props: {
-    value: {
+    modelValue: {
       type: Array,
       required: true,
     },
@@ -88,76 +88,115 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      localValue: this.value,
-      attributes: [],
-      model: 'attributes',
-    }
-  },
-  watch: {
-    localValue(newValue) {
-      if (newValue === null) newValue = []
-      this.$emit('input', newValue)
-    },
-    value: {
-      handler: function (val, oldVal) {
-        this.localValue = val
-        this.updateAttributes()
-      },
-      deep: true,
-    },
-  },
-  mounted() {
-    this.updateAttributes()
-  },
-  methods: {
-    async filterAttributes(val, update, abort) {
+  emits: ['update:model-value'],
+  setup(props, { emit }) {
+    const { copyList, pasteList, hasPaste } = useCopyPaste()
+    const { elementIcon, equivalentModel, equivalentKey, attributeValue } =
+      useElement()
+
+    const attributesComponent = ref(null)
+
+    const model = ref('attributes')
+    const attributes = reactive([])
+    const localValue = ref(props.modelValue)
+
+    onMounted(() => {
+      updateAttributes()
+    })
+
+    const filterAttributes = async (val, update, abort) => {
       // call abort() at any time if you can't retrieve data somehow
       if (val.length < 3) {
         abort()
         return
       }
 
-      await this.$axios
-        .get(`/api/v1/token/${this.model}/`, {
+      await api
+        .get(`/api/v1/token/${model.value}/`, {
           params: { search: val.toLowerCase() },
         })
         .then((response) => {
-          this.attributes = response.data.results
+          Object.assign(attributes, response.data.results)
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
         })
 
       update(() => {})
-    },
+    }
 
-    abortFilterAttributes() {
+    const abortFilterAttributes = () => {
       // console.log('delayed filter aborted')
-    },
+    }
 
-    updateAttributes() {
-      Object.entries(this.value).map(([key, val]) => {
+    const updateAttributes = () => {
+      if (localValue.value === null) {
+        localValue.value = []
+        return
+      }
+
+      Object.entries(localValue.value).map(([key, val]) => {
         if (
           !('status' in val) &&
+          'property_att' in val &&
           (val.property_att.prefix === 'CID' ||
             val.property_att.prefix === 'SET' ||
             val.property_att.prefix === 'DMN')
         ) {
-          this.$axios
-            .get(`/api/v1/token/${this.model}/${val.id}/badge/`)
+          api
+            .get(`/api/v1/token/${model.value}/${val.id}/badge/`)
             .then((response) => {
-              this.$set(
-                this.value,
-                key,
-                Object.assign({}, this.value[key], response.data)
+              localValue.value[key] = Object.assign(
+                {},
+                localValue.value[key],
+                response.data
               )
-              this.$set(this.value[key], 'description', response.data.text)
+              localValue.value[key].description = response.data.text
+            })
+            .catch((error) => {
+              uiStore.notifyError(error)
             })
         }
-        if (!('status' in val) && val.property_att.sort === 'server') {
-          this.$set(this.value[key], 'status', 'tag')
+        if (
+          !('status' in val) &&
+          'property_att' in val &&
+          val.property_att.sort === 'server'
+        ) {
+          localValue.value[key].status = 'tag'
         }
       })
-    },
+      emit('update:model-value', localValue)
+    }
+
+    watch(
+      () => props.modelValue,
+      (newValue) => {
+        localValue.value = newValue
+        updateAttributes()
+      }
+    )
+
+    watch(localValue, (newValue) => {
+      if (newValue === null) newValue = []
+      emit('update:model-value', newValue)
+    })
+
+    return {
+      attributesComponent,
+      model,
+      attributes,
+      localValue,
+      copyList,
+      pasteList,
+      hasPaste,
+      elementIcon,
+      equivalentModel,
+      equivalentKey,
+      attributeValue,
+      filterAttributes,
+      abortFilterAttributes,
+      updateAttributes,
+    }
   },
 }
 </script>
