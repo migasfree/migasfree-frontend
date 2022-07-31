6fe9939ba6fbd6dd4b384d5148f2942f2fff1843
jact <jachavar@gmail.com>
2022-07-31 11:39:54 +0200

    Upgraded code to Vue3 and Quasar2 (#1)

--- src/components/map/AddLocation.vue
+++ src/components/map/AddLocation.vue
@@ -1,46 +1,64 @@
 <template>
   <l-map id="map" :zoom="zoom" :center="coords" @click="updateMarker">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="coords" :icon="iconMarker"></l-marker>
+    <l-marker :lat-lng="coords">
+      <l-icon
+        :icon-url="iconUrl"
+        :icon-size="iconSize"
+        :icon-anchor="iconAnchor"
+      />
+    </l-marker>
   </l-map>
 </template>
 
 <script>
+import { ref, watch } from 'vue'
+
 export default {
   name: 'AddLocation',
   props: {
-    value: {
+    modelValue: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
-  data() {
-    return {
-      zoom: 16,
-      url: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      coords: this.value || [0, 0],
-      attribution:
-        '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-      iconMarker: L.icon({
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
-        iconSize: [32, 40],
-        iconAnchor: [16, 37]
-      })
+  emits: ['update-coords'],
+  setup(props, { emit }) {
+    const zoom = 16
+    const url = 'https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    const attribution =
+      '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+
+    const coords = ref(props.modelValue || [0, 0])
+
+    const iconUrl = require('leaflet/dist/images/marker-icon.png')
+    const iconSize = [32, 40]
+    const iconAnchor = [16, 37]
+
+    const updateMarker = ($evt) => {
+      if ($evt.latlng === undefined) return
+      coords.value = [$evt.latlng.lat, $evt.latlng.lng]
+      emit('update-coords', coords.value)
     }
-  },
-  watch: {
-    value: {
-      handler: function(val, oldVal) {
-        this.coords = val
+
+    watch(
+      () => props.modelValue,
+      (val) => {
+        coords.value = val
       }
+    )
+
+    return {
+      zoom,
+      url,
+      attribution,
+      coords,
+      iconUrl,
+      iconSize,
+      iconAnchor,
+      updateMarker,
     }
   },
-  methods: {
-    updateMarker($evt) {
-      this.coords = [$evt.latlng.lat, $evt.latlng.lng]
-      this.$emit('update-coords', this.coords)
-    }
-  }
 }
 </script>
 
