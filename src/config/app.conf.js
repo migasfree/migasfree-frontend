743fd37e1dfc8bc0c9ca56b679d7347cf05818d9
jact <jachavar@gmail.com>
2022-07-31 11:42:26 +0200

    Removed comments

--- src/config/app.conf.js
+++ src/config/app.conf.js
@@ -1,41 +1,5 @@
 export const MIGASFREE_SECONDS_MESSAGE_ALERT = 1800
 
-/*
-export const MIGASFREE_CHART_COLORS = [
-  // coolors.co
-  '#294C60', // charcoal
-  '#ff8552', // coral
-  '#297373', // skobeloff
-  '#DB3A34', // cg red
-  '#084C61', // midnight green eagle green
-  '#39393A', // onyx
-  '#FFC857', // maximum yellow red
-  '#613A3A', // rose ebony
-  '#85FFC7', // aquamarine
-  '#573280', // spanish violet
-  '#ADA8B6', // silver metallic
-  '#845A6D', // mauve taupe
-  '#3D3B8E', // dark slate blue
-  '#FFC49B' // peach crayola
-]
-*/
-
-// https://colorbrewer2.org/?type=qualitative&scheme=Set3&n=12
-/* export const MIGASFREE_CHART_COLORS = [
-  '#a6cee3',
-  '#1f78b4',
-  '#b2df8a',
-  '#33a02c',
-  '#fb9a99',
-  '#e31a1c',
-  '#fdbf6f',
-  '#ff7f00',
-  '#cab2d6',
-  '#6a3d9a',
-  '#ffff99',
-  '#b15928'
-] */
-
 export const MIGASFREE_CHART_COLORS = [
   '#5f98cf',
   '#434348',
@@ -46,7 +10,7 @@ export const MIGASFREE_CHART_COLORS = [
   '#397550',
   '#c0493d',
   '#4f4a7a',
-  '#b381b3'
+  '#b381b3',
 ]
 
 export const MIGASFREE_CHART_DARK_COLORS = [
@@ -59,16 +23,6 @@ export const MIGASFREE_CHART_DARK_COLORS = [
   '#f9ce1d',
   '#33b2df',
   '#d4526e',
-  '#f9a3a4'
+  '#f9a3a4',
 ]
 
-/* export const MIGASFREE_CHART_COLORS = [
-  '#514F78',
-  '#42A07B',
-  '#9B5E4A',
-  '#72727F',
-  '#1F949A',
-  '#82914E',
-  '#86777F',
-  '#42A07B'
-] */
