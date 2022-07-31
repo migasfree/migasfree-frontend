9b40c18257854062919d75f98e87a506b34594fa
jact <jachavar@gmail.com>
2022-07-31 12:09:06 +0200

    Upgraded code to Vue3 (#1)

--- src/boot/leaflet.js
+++ src/boot/leaflet.js
@@ -1,9 +1,18 @@
-import Vue from 'vue'
-import { LMap, LTileLayer, LMarker, LTooltip, LControl } from 'vue2-leaflet'
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LTooltip,
+  LControl,
+  LIcon,
+} from '@vue-leaflet/vue-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-Vue.component('LMap', LMap)
-Vue.component('LTileLayer', LTileLayer)
-Vue.component('LMarker', LMarker)
-Vue.component('LTooltip', LTooltip)
-Vue.component('LControl', LControl)
+export default async ({ app }) => {
+  app.component('LMap', LMap)
+  app.component('LTileLayer', LTileLayer)
+  app.component('LMarker', LMarker)
+  app.component('LTooltip', LTooltip)
+  app.component('LControl', LControl)
+  app.component('LIcon', LIcon)
+}
