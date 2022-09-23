383548f54c109b5c7c8e9cd2b2149899fe17e3b9
Jose Antonio Chavarría <jachavar@gmail.com>
2022-09-23 12:14:22 +0200

    Added new rules to disable

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,14 @@ module.exports = {
   root: true,
 
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
   },
 
   env: {
     browser: true,
-    "vue/setup-compiler-macros": true,
+    'vue/setup-compiler-macros': true,
   },
 
   // Rules order is important, please avoid shuffling them
@@ -25,18 +25,18 @@ module.exports = {
     // See https://eslint.vuejs.org/rules/#available-rules
     // 'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
     // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    "plugin:vue/vue3-recommended", // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     // https://github.com/prettier/eslint-config-prettier#installation
     // usage with Prettier, provided by 'eslint-config-prettier'.
-    "prettier",
+    'prettier',
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
     // required to lint *.vue files
-    "vue",
-    "prettier",
+    'vue',
+    'prettier',
 
     // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
     // Prettier has not been included as plugin to avoid performance impact
@@ -44,29 +44,31 @@ module.exports = {
   ],
 
   globals: {
-    ga: "readonly", // Google Analytics
-    cordova: "readonly",
-    __statics: "readonly",
-    __QUASAR_SSR__: "readonly",
-    __QUASAR_SSR_SERVER__: "readonly",
-    __QUASAR_SSR_CLIENT__: "readonly",
-    __QUASAR_SSR_PWA__: "readonly",
-    process: "readonly",
-    Capacitor: "readonly",
-    chrome: "readonly",
+    ga: 'readonly', // Google Analytics
+    cordova: 'readonly',
+    __statics: 'readonly',
+    __QUASAR_SSR__: 'readonly',
+    __QUASAR_SSR_SERVER__: 'readonly',
+    __QUASAR_SSR_CLIENT__: 'readonly',
+    __QUASAR_SSR_PWA__: 'readonly',
+    process: 'readonly',
+    Capacitor: 'readonly',
+    chrome: 'readonly',
   },
 
   // add your custom rules here
   rules: {
-    "prefer-promise-reject-errors": "off",
-    "object-shorthand": 0,
-    "vue/no-v-html": 0,
-    "vue/multi-word-component-names": "off",
-    "vue/no-deprecated-slot-attribute": "off",
-    "vue/no-deprecated-slot-scope-attribute": "off",
-    "vue/no-deprecated-v-bind-sync": "off",
+    'prefer-promise-reject-errors': 'off',
+    'object-shorthand': 0,
+    'vue/no-v-html': 0,
+    'vue/multi-word-component-names': 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/no-deprecated-slot-scope-attribute': 'off',
+    'vue/no-deprecated-v-bind-sync': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/valid-attribute-name': 'off',
 
     // allow debugger during development only
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-};
+}
