8e17e3ba402e6f882389fb54d402c28f5c5a3f75
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-13 12:08:58 +0200

    Added DateView component

---
+++ src/components/ui/DateView.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-icon v-if="icon" :name="icon" size="sm" class="vertical-middle" />
+  <span v-if="value" class="vertical-middle">
+    {{ showDate(value) }}
+    <q-tooltip>
+      <template v-if="tooltipText">
+        {{ tooltipText }} ({{ diffForHumans(value) }})
+      </template>
+      <template v-else>{{ diffForHumans(value) }} </template></q-tooltip
+    >
+  </span>
+</template>
+
+<script>
+import useDate from 'composables/date'
+
+export default {
+  name: 'DateView',
+
+  props: {
+    value: {
+      type: [String, null],
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    tooltipText: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+
+  setup() {
+    const { showDate, diffForHumans } = useDate()
+
+    return { showDate, diffForHumans }
+  },
+}
+</script>
