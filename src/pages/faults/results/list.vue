650c6b00870ecc6ffd75000baa92e8bd8693f12e
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-13 12:10:31 +0200

    Use of DateView component

--- src/pages/faults/results/list.vue
+++ src/pages/faults/results/list.vue
@@ -39,10 +39,7 @@
         </span>
 
         <span v-else-if="slotProps.props.column.field == 'created_at'">
-          {{ showDate(slotProps.props.row.created_at) }}
-          <q-tooltip>{{
-            diffForHumans(slotProps.props.row.created_at)
-          }}</q-tooltip>
+          <DateView :value="slotProps.props.row.created_at" />
         </span>
 
         <span v-else-if="slotProps.props.column.field == 'result'">
@@ -72,10 +69,10 @@ import { useUiStore } from 'stores/ui'
 import Breadcrumbs from 'components/ui/Breadcrumbs'
 import TableResults from 'components/ui/TableResults'
 import BooleanView from 'components/ui/BooleanView'
+import DateView from 'components/ui/DateView'
 import Truncate from 'components/ui/Truncate'
 import MigasLink from 'components/MigasLink'
 
-import useDate from 'composables/date'
 import { useElement, modelIcon } from 'composables/element'
 
 export default {
@@ -83,13 +80,13 @@ export default {
     Breadcrumbs,
     TableResults,
     BooleanView,
+    DateView,
     Truncate,
     MigasLink,
   },
   setup() {
     const { $gettext } = useGettext()
     const uiStore = useUiStore()
-    const { showDate, diffForHumans } = useDate()
     const { elementIcon } = useElement()
 
     useMeta({ title: $gettext('Faults List') })
@@ -256,8 +253,6 @@ export default {
       title,
       breadcrumbs,
       columns,
-      showDate,
-      diffForHumans,
       elementIcon,
     }
   },
