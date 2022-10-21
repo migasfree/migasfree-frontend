7b251317ac4aa044017ae35e3048f2e58b5b9b95
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-21 09:47:22 +0200

    Added DayInput component

---
+++ src/components/ui/DayInput.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-input
+    v-model="dayPicked"
+    :readonly="readonly"
+    outlined
+    :dense="label !== undefined"
+    clearable
+    input-class="cursor-pointer"
+    :label="label"
+    @click="dayPicker.show()"
+  >
+    <template #append>
+      <q-icon name="event" class="cursor-pointer">
+        <q-popup-proxy
+          ref="dayPicker"
+          transition-show="scale"
+          transition-hide="scale"
+        >
+          <q-date
+            v-model="dayPicked"
+            mask="YYYY-MM-DD"
+            mode="date"
+            landscape
+            outlined
+            today-btn
+            :locale="localeDate"
+            @update:model-value="updateValue"
+          />
+        </q-popup-proxy>
+      </q-icon>
+    </template>
+
+    <q-tooltip v-if="dayPicked">{{ diffForHumans(dayPicked) }}</q-tooltip>
+  </q-input>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+
+import useDate from 'composables/date'
+
+export default {
+  name: 'DayInput',
+  props: {
+    modelValue: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    label: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+  emits: ['update:model-value'],
+  setup(props, { emit }) {
+    const { localeDate, diffForHumans } = useDate()
+
+    const dayPicker = ref(null)
+
+    const dayPicked = computed({
+      get: () => props.modelValue,
+      set: (val) => {
+        emit('update:model-value', val)
+      },
+    })
+
+    const updateValue = (val, reason, details) => {
+      if (reason === 'add-day') {
+        dayPicker.value.hide()
+        emit('update:model-value', val)
+      }
+    }
+
+    return {
+      dayPicked,
+      dayPicker,
+      localeDate,
+      diffForHumans,
+      updateValue,
+    }
+  },
+}
+</script>
