24616699f23e20a9897eed46ed3f85971901bd3b
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-24 12:54:25 +0200

    Added last hours selector in events history

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -37,36 +37,58 @@
 
     <div class="row">
       <div class="col-12">
-        <q-list id="events-history" class="q-ma-sm rounded-borders" bordered>
+        <q-list
+          id="events-history"
+          class="q-ma-sm q-pa-none rounded-borders shadow-1"
+          bordered
+        >
           <q-expansion-item @show="loadEventsHistory">
             <template #header>
               <q-item-section>
                 <div class="text-h5">
-                  <translate>History of events in the last 72 hours</translate>
+                  <translate :translate-params="{ number: lastHours }"
+                    >History of events in the last %{ number } hours</translate
+                  >
                 </div>
               </q-item-section>
             </template>
 
             <q-card>
-              <div v-if="loading" class="text-center">
-                <q-spinner-dots color="primary" size="3em" />
-              </div>
-              <StackedBarChart
-                v-show="!loading"
-                title=""
-                :initial-data="eventsHistory"
-                @get-link="goTo"
-              />
+              <q-card-section>
+                <div v-if="loading" class="text-center">
+                  <q-spinner-dots color="primary" size="3em" />
+                </div>
 
-              <q-card-actions align="right">
-                <q-btn
-                  icon="mdi-refresh"
-                  :disabled="loading"
-                  :loading="loading"
-                  :label="$gettext('Update')"
-                  @click="updateEventsHistory"
-                />
-              </q-card-actions>
+                <StackedBarChart
+                  v-show="!loading"
+                  title=""
+                  borderless
+                  :initial-data="eventsHistory"
+                  @get-link="goTo"
+                >
+                  <template #selector>
+                    <q-card-section class="row justify-center q-py-none">
+                      <q-input
+                        v-model="lastHours"
+                        type="number"
+                        class="q-ma-sm"
+                        outlined
+                        dense
+                        :label="$gettext('Last Hours')"
+                      />
+
+                      <q-btn
+                        icon="mdi-refresh"
+                        class="q-ma-sm"
+                        :disabled="loading"
+                        :loading="loading"
+                        :label="$gettext('Update')"
+                        @click="updateEventsHistory"
+                      />
+                    </q-card-section>
+                  </template>
+                </StackedBarChart>
+              </q-card-section>
             </q-card>
           </q-expansion-item>
         </q-list>
@@ -170,6 +192,7 @@ export default defineComponent({
     const loadingMonthlySyncs = ref(false)
     const eventsHistory = reactive({})
 
+    const lastHours = ref(72)
     const begin = ref('')
     const end = ref('')
 
@@ -253,35 +276,48 @@ export default defineComponent({
       })
     }
 
+    const calculateBeginDate = () => {
+      if (lastHours.value <= 0) return ''
+
+      let date = new Date()
+      date.setHours(date.getHours() - lastHours.value)
+
+      return `${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()}T${date.getHours()}`
+    }
+
     const loadEventsHistory = async () => {
+      const begin = calculateBeginDate()
+
       loading.value = true
 
       const syncs = await api
-        .get('/api/v1/token/stats/syncs/history/')
+        .get('/api/v1/token/stats/syncs/history/', { params: { begin } })
         .catch((error) => {
           uiStore.notifyError(error)
         })
 
       const errors = await api
-        .get('/api/v1/token/stats/errors/history/')
+        .get('/api/v1/token/stats/errors/history/', { params: { begin } })
         .catch((error) => {
           uiStore.notifyError(error)
         })
 
       const faults = await api
-        .get('/api/v1/token/stats/faults/history/')
+        .get('/api/v1/token/stats/faults/history/', { params: { begin } })
         .catch((error) => {
           uiStore.notifyError(error)
         })
 
       const migrations = await api
-        .get('/api/v1/token/stats/migrations/history/')
+        .get('/api/v1/token/stats/migrations/history/', { params: { begin } })
         .catch((error) => {
           uiStore.notifyError(error)
         })
 
       const statusLogs = await api
-        .get('/api/v1/token/stats/status-logs/history/')
+        .get('/api/v1/token/stats/status-logs/history/', { params: { begin } })
         .catch((error) => {
           uiStore.notifyError(error)
         })
@@ -400,6 +436,7 @@ export default defineComponent({
       projects,
       loading,
       loadingMonthlySyncs,
+      lastHours,
       begin,
       end,
       eventsHistory,
