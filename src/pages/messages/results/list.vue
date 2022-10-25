9a66df6faec783b3842d9cf9d24e9b8a345601da
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-25 13:06:04 +0200

    Improved design

--- src/pages/messages/results/list.vue
+++ src/pages/messages/results/list.vue
@@ -12,7 +12,7 @@
       <template #top>
         <q-btn
           icon="mdi-refresh"
-          class="q-ma-md"
+          class="q-my-md"
           :loading="loading"
           :disable="loading"
           :label="$gettext('Update')"
