62edbaf70466b89effb9896f310eb66229c4559f
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-22 14:22:21 +0100

    refactor: setup store syntax

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,3 +1,4 @@
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { LocalStorage } from 'quasar'
 
@@ -5,239 +6,253 @@ import { api } from 'boot/axios'
 import { gettext } from 'boot/gettext'
 import { useUiStore } from './ui'
 
-export const useAuthStore = defineStore('auth', {
-  id: 'auth',
-  state: () => ({
-    token: LocalStorage.getItem('auth.token') || '',
-    loggedIn: LocalStorage.getItem('auth.loggedIn') || false,
-    user: LocalStorage.getItem('auth.user') || {},
-    server: LocalStorage.getItem('auth.server') || {},
-    domains: LocalStorage.getItem('auth.domains') || [
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref(LocalStorage.getItem('auth.token')) || ref('')
+  const loggedIn = ref(LocalStorage.getItem('auth.loggedIn')) || ref(false)
+  const user = ref(LocalStorage.getItem('auth.user')) || ref({})
+  const server = ref(LocalStorage.getItem('auth.server')) || ref({})
+  const domains =
+    ref(LocalStorage.getItem('auth.domains')) ||
+    ref([
       {
         id: 0,
         name: gettext.$gettext('All').toUpperCase(),
       },
-    ],
-    scopes: LocalStorage.getItem('auth.scopes') || [
+    ])
+  const scopes =
+    ref(LocalStorage.getItem('auth.scopes')) ||
+    ref([
       {
         id: 0,
         name: gettext.$gettext('All').toLowerCase(),
       },
-    ],
-  }),
-  getters: {
-    getToken: (state) => state.token,
-    isLoggedIn: (state) => state.loggedIn,
-    getDomains: (state) => state.domains,
-    getScopes: (state) => state.scopes,
-    filteredScopes: (state) => {
-      if ('domain_preference' in state.user && state.user.domain_preference) {
-        return state.scopes.filter(
-          (el) =>
-            (el.domain && el.domain.id === state.user.domain_preference.id) ||
-            el.id === 0
-        )
-      }
+    ])
 
-      return state.scopes
-    },
-  },
-  actions: {
-    async login(data) {
-      const uiStore = useUiStore()
+  const filteredScopes = computed(() => {
+    if ('domain_preference' in user.value && user.value.domain_preference) {
+      return scopes.value.filter(
+        (el) =>
+          (el.domain && el.domain.id === user.value.domain_preference.id) ||
+          el.id === 0
+      )
+    }
 
-      await api
-        .post('/rest-auth/login/', data)
-        .then((response) => {
-          this.setToken(response.data.key)
-          this.getUser()
-          if (this.token) {
-            this.setLoggedIn(true)
-            this.getServerInfo()
-            this.loadDomains()
-            this.loadScopes()
-          }
+    return scopes.value
+  })
+
+  async function login(data) {
+    const uiStore = useUiStore()
+
+    await api
+      .post('/rest-auth/login/', data)
+      .then((response) => {
+        setToken(response.data.key)
+        getUser()
+        if (token.value) {
+          setLoggedIn(true)
+          getServerInfo()
+          loadDomains()
+          loadScopes()
+        }
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  function getUser() {
+    const uiStore = useUiStore()
+
+    api
+      .get('/rest-auth/user/')
+      .then((response) => {
+        if (response.data.is_staff === false) {
+          reset()
+          throw new Error(gettext.$gettext('Invalid credentials'))
+        }
+
+        setUser(response.data)
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  async function getServerInfo() {
+    const uiStore = useUiStore()
+
+    await api
+      .get('/api/v1/public/server/info/')
+      .then((response) => {
+        setServerInfo(response.data)
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  async function logout() {
+    const uiStore = useUiStore()
+
+    await api
+      .post('/rest-auth/logout/')
+      .then((response) => {
+        reset()
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  function reset() {
+    setToken('')
+    setLoggedIn(false)
+    resetUser()
+    resetServerInfo()
+    resetDomains()
+    resetScopes()
+  }
+
+  function addDomain(payload) {
+    if (!domains.value.find((el) => el.name === payload.name)) {
+      deleteDomain(payload.id)
+      domains.value.push(payload)
+      domains.value.sort((a, b) =>
+        a.id && b.id && a.name > b.name
+          ? 1
+          : a.id && b.id && b.name > a.name
+          ? -1
+          : 0
+      )
+      LocalStorage.set('auth.domains', domains.value)
+    }
+  }
+
+  async function loadDomains() {
+    const uiStore = useUiStore()
+
+    await api
+      .get('/api/v1/token/domains/')
+      .then((response) => {
+        Object.entries(response.data.results).map(([index, item]) => {
+          addDomain({ id: item.id, name: item.name })
         })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
 
-    getUser() {
-      const uiStore = useUiStore()
+  function addScope(payload) {
+    if (!scopes.value.find((el) => el.name === payload.name)) {
+      deleteScope(payload.id)
+      scopes.value.push(payload)
+      scopes.value.sort((a, b) =>
+        a.id && b.id && a.name > b.name
+          ? 1
+          : a.id && b.id && b.name > a.name
+          ? -1
+          : 0
+      )
+      LocalStorage.set('auth.scopes', scopes.value)
+    }
+  }
 
-      api
-        .get('/rest-auth/user/')
-        .then((response) => {
-          if (response.data.is_staff === false) {
-            this.reset()
-            throw new Error(gettext.$gettext('Invalid credentials'))
-          }
+  async function loadScopes() {
+    const uiStore = useUiStore()
 
-          this.setUser(response.data)
-        })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
-
-    async getServerInfo() {
-      const uiStore = useUiStore()
-
-      await api
-        .get('/api/v1/public/server/info/')
-        .then((response) => {
-          this.setServerInfo(response.data)
-        })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
-
-    async logout() {
-      const uiStore = useUiStore()
-
-      await api
-        .post('/rest-auth/logout/')
-        .then((response) => {
-          this.reset()
-        })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
-
-    reset() {
-      this.setToken('')
-      this.setLoggedIn(false)
-      this.resetUser()
-      this.resetServerInfo()
-      this.resetDomains()
-      this.resetScopes()
-    },
-
-    addDomain(payload) {
-      if (!this.domains.find((el) => el.name === payload.name)) {
-        this.deleteDomain(payload.id)
-        this.domains.push(payload)
-        this.domains.sort((a, b) =>
-          a.id && b.id && a.name > b.name
-            ? 1
-            : a.id && b.id && b.name > a.name
-            ? -1
-            : 0
-        )
-        LocalStorage.set('auth.domains', this.domains)
-      }
-    },
-
-    async loadDomains() {
-      const uiStore = useUiStore()
-
-      await api
-        .get('/api/v1/token/domains/')
-        .then((response) => {
-          Object.entries(response.data.results).map(([index, item]) => {
-            this.addDomain({ id: item.id, name: item.name })
+    await api
+      .get('/api/v1/token/scopes/', { user__id: this.user.id })
+      .then((response) => {
+        Object.entries(response.data.results).map(([index, item]) => {
+          addScope({
+            id: item.id,
+            name: item.name,
+            domain: item.domain,
           })
         })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
 
-    addScope(payload) {
-      if (!this.scopes.find((el) => el.name === payload.name)) {
-        this.deleteScope(payload.id)
-        this.scopes.push(payload)
-        this.scopes.sort((a, b) =>
-          a.id && b.id && a.name > b.name
-            ? 1
-            : a.id && b.id && b.name > a.name
-            ? -1
-            : 0
-        )
-        LocalStorage.set('auth.scopes', this.scopes)
-      }
-    },
+  function setLoggedIn(value) {
+    loggedIn.value = value
+    LocalStorage.set('auth.loggedIn', loggedIn.value)
+  }
 
-    async loadScopes() {
-      const uiStore = useUiStore()
+  function setUser(value) {
+    user.value = value
+    LocalStorage.set('auth.user', user.value)
+  }
 
-      await api
-        .get('/api/v1/token/scopes/', { user__id: this.user.id })
-        .then((response) => {
-          Object.entries(response.data.results).map(([index, item]) => {
-            this.addScope({
-              id: item.id,
-              name: item.name,
-              domain: item.domain,
-            })
-          })
-        })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
+  function setServerInfo(value) {
+    server.value = value
+    LocalStorage.set('auth.server', server.value)
+  }
 
-    setLoggedIn(value) {
-      this.loggedIn = value
-      LocalStorage.set('auth.loggedIn', this.loggedIn)
-    },
+  function setToken(value) {
+    token.value = value
+    LocalStorage.set('auth.token', token.value)
+  }
 
-    setUser(value) {
-      this.user = value
-      LocalStorage.set('auth.user', this.user)
-    },
+  function resetUser() {
+    user.value = {}
+    LocalStorage.set('auth.user', user.value)
+  }
 
-    setServerInfo(value) {
-      this.server = value
-      LocalStorage.set('auth.server', this.server)
-    },
+  function resetServerInfo() {
+    server.value = {}
+    LocalStorage.set('auth.server', server.value)
+  }
 
-    setToken(value) {
-      this.token = value
-      LocalStorage.set('auth.token', this.token)
-    },
+  function deleteDomain(id) {
+    const removeIndex = domains.value.map((item) => item.id).indexOf(id)
+    if (removeIndex >= 0) domains.value.splice(removeIndex, 1)
+  }
 
-    resetUser() {
-      this.user = {}
-      LocalStorage.set('auth.user', this.user)
-    },
+  function resetDomains() {
+    domains.value = [
+      {
+        id: 0,
+        name: gettext.$gettext('All').toUpperCase(),
+      },
+    ]
+    LocalStorage.set('auth.domains', domains.value)
+  }
 
-    resetServerInfo() {
-      this.server = {}
-      LocalStorage.set('auth.server', this.server)
-    },
+  function deleteScope(id) {
+    const removeIndex = scopes.value.map((item) => item.id).indexOf(id)
+    if (removeIndex >= 0) scopes.value.splice(removeIndex, 1)
+  }
 
-    deleteDomain(id) {
-      const removeIndex = this.domains.map((item) => item.id).indexOf(id)
-      if (removeIndex >= 0) this.domains.splice(removeIndex, 1)
-    },
+  function resetScopes() {
+    scopes.value = [
+      {
+        id: 0,
+        name: gettext.$gettext('All').toLowerCase(),
+      },
+    ]
+    LocalStorage.set('auth.scopes', scopes.value)
+  }
 
-    resetDomains() {
-      this.domains = [
-        {
-          id: 0,
-          name: gettext.$gettext('All').toUpperCase(),
-        },
-      ]
-      LocalStorage.set('auth.domains', this.domains)
-    },
-
-    deleteScope(id) {
-      const removeIndex = this.scopes.map((item) => item.id).indexOf(id)
-      if (removeIndex >= 0) this.scopes.splice(removeIndex, 1)
-    },
-
-    resetScopes() {
-      this.scopes = [
-        {
-          id: 0,
-          name: gettext.$gettext('All').toLowerCase(),
-        },
-      ]
-      LocalStorage.set('auth.scopes', this.scopes)
-    },
-  },
+  return {
+    token,
+    loggedIn,
+    user,
+    server,
+    domains,
+    scopes,
+    filteredScopes,
+    login,
+    logout,
+    reset,
+    addDomain,
+    addScope,
+    loadScopes,
+    setUser,
+    deleteDomain,
+    deleteScope,
+  }
 })
