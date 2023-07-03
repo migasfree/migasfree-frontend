8b8ea5e5b568036998edc1d71ab7072850422c36
jact <jachavar@gmail.com>
2023-07-03 08:08:20 +0200

    feat(singularity): added pages

---
+++ src/pages/singularities/results/detail.vue
@@ -0,0 +1,282 @@
+<template>
+  <q-page padding>
+    <ItemDetail
+      :breadcrumbs="breadcrumbs"
+      :original-title="title"
+      :model="model"
+      :routes="routes"
+      :element="element"
+      :element-data="elementData"
+      :is-valid="isValid"
+      @load-related="loadRelated"
+      @reset-element="resetElement"
+      @set-title="setTitle"
+    >
+      <template #fields>
+        <q-card-section>
+          <div v-translate class="text-h5 q-mt-sm q-mb-xs">General</div>
+
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-4 col-md col-sm">
+              <q-checkbox
+                v-model="element.enabled"
+                left-label
+                autofocus
+                :label="$gettext('Enabled?')"
+              />
+            </div>
+
+            <div class="col-4 col-md col-sm">
+              <q-select
+                v-model="element.property_att"
+                outlined
+                :label="$gettext('Formula')"
+                :options="formulas"
+                option-value="id"
+                option-label="name"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              >
+                <template #prepend>
+                  <q-icon :name="modelIcon('formulas')" />
+                </template>
+              </q-select>
+            </div>
+
+            <div class="col-4 col-md col-sm">
+              <q-input
+                v-model="element.priority"
+                outlined
+                type="number"
+                :label="$gettext('Priority')"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div v-translate class="text-h5 q-mt-sm q-mb-xs">Attributes</div>
+
+          <div class="row q-pa-md q-gutter-md">
+            <div class="col-6 col-md col-sm">
+              <SelectAttributes
+                v-model="element.included_attributes"
+                :label="$gettext('Included')"
+              />
+            </div>
+
+            <div class="col-6 col-md col-sm">
+              <SelectAttributes
+                v-model="element.excluded_attributes"
+                :label="$gettext('Excluded')"
+              />
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div v-translate class="text-h5 q-mt-sm q-mb-xs">Code</div>
+
+          <div class="row q-pa-md q-gutter-md justify-between q-mb-xl">
+            <div class="col-md-3 col-sm-3 col-xs-3">
+              <q-select
+                v-model="element.language"
+                outlined
+                :label="$gettext('Language')"
+                :options="languages"
+                option-value="id"
+                option-label="name"
+                lazy-rules
+                :rules="[(val) => !!val || $gettext('* Required')]"
+              />
+            </div>
+
+            <div class="col-md-8 col-sm-8 col-xs-8 q-mb-xl">
+              <CodeEditor v-model="element.code" :language="highlightLang" />
+
+              <q-input borderless dense bottom-slots readonly>
+                <template #hint>
+                  <p v-translate>
+                    This code will execute in the client computer, and it must
+                    put in the standard output the value of the attribute
+                    correspondent to this property.
+                  </p>
+                  <p v-translate>
+                    The format of this value is 'name~description', where
+                    'description' is optional.
+                  </p>
+                  <p><b v-translate>Example of code:</b></p>
+                  <p v-translate>
+                    # Create an attribute with the name of computer from bash
+                  </p>
+                  <p><code>echo $HOSTNAME</code></p>
+                </template>
+              </q-input>
+            </div>
+          </div>
+        </q-card-section>
+      </template>
+    </ItemDetail>
+  </q-page>
+</template>
+
+<script>
+import { ref, reactive, computed } from 'vue'
+import { useGettext } from 'vue3-gettext'
+import { useMeta } from 'quasar'
+
+import { api } from 'boot/axios'
+import { useUiStore } from 'stores/ui'
+
+import ItemDetail from 'components/ui/ItemDetail'
+import SelectAttributes from 'components/ui/SelectAttributes'
+import CodeEditor from 'components/ui/CodeEditor'
+
+import { appIcon, modelIcon } from 'composables/element'
+
+export default {
+  components: {
+    ItemDetail,
+    SelectAttributes,
+    CodeEditor,
+  },
+  setup() {
+    const uiStore = useUiStore()
+    const { $gettext } = useGettext()
+
+    const title = ref($gettext('Singularity'))
+    const windowTitle = ref(title.value)
+    useMeta(() => {
+      return {
+        title: windowTitle.value,
+      }
+    })
+
+    const routes = {
+      list: 'singularities-list',
+      add: 'singularity-add',
+      detail: 'singularity-detail',
+    }
+    const model = 'singularities'
+
+    let element = reactive({
+      id: 0,
+      enabled: false,
+      included_attributes: [],
+      excluded_attributes: [],
+      code: '',
+    })
+
+    const breadcrumbs = reactive([
+      {
+        text: $gettext('Dashboard'),
+        icon: appIcon('home'),
+        to: 'home',
+      },
+      {
+        text: $gettext('Configuration'),
+        icon: appIcon('configuration'),
+      },
+      {
+        text: $gettext('Singularities'),
+        icon: modelIcon(model),
+        to: routes.list,
+      },
+    ])
+
+    const formulas = ref([])
+    const languages = ref([])
+
+    const isValid = computed(() => {
+      return (
+        element.property_att !== undefined &&
+        element.language !== undefined &&
+        element.code !== undefined &&
+        element.code.trim() !== ''
+      )
+    })
+
+    const highlightLang = computed(() => {
+      if ('language' in element && typeof element.language === 'object')
+        return element.language.name
+      return 'python'
+    })
+
+    const loadRelated = async () => {
+      await api
+        .get('/api/v1/token/formulas/')
+        .then((response) => {
+          formulas.value = response.data.results
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+
+      await api
+        .get(`/api/v1/public/languages/`)
+        .then((response) => {
+          Object.entries(response.data).map(([key, val]) => {
+            languages.value.push({
+              id: parseInt(key),
+              name: val,
+            })
+          })
+          if (element.id && typeof element.language === 'number')
+            element.language = languages.value.find(
+              (x) => x.id === element.language
+            )
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+    }
+
+    const elementData = () => {
+      return {
+        enabled: element.enabled,
+        priority: element.priority,
+        property_att: element.property_att.id,
+        included_attributes: element.included_attributes.map((item) => item.id),
+        excluded_attributes: element.excluded_attributes.map((item) => item.id),
+        language: element.language.id,
+        code: element.code,
+      }
+    }
+
+    const resetElement = () => {
+      Object.assign(element, {
+        id: 0,
+        property_att: undefined,
+        priority: 0,
+        included_attributes: [],
+        excluded_attributes: [],
+        language: undefined,
+        code: '',
+      })
+    }
+
+    const setTitle = (value) => {
+      windowTitle.value = value
+    }
+
+    return {
+      breadcrumbs,
+      title,
+      model,
+      routes,
+      element,
+      formulas,
+      languages,
+      isValid,
+      highlightLang,
+      loadRelated,
+      elementData,
+      resetElement,
+      setTitle,
+      modelIcon,
+    }
+  },
+}
+</script>
---
+++ src/pages/singularities/results/list.vue
@@ -0,0 +1,179 @@
+<template>
+  <q-page padding>
+    <Breadcrumbs :items="breadcrumbs" />
+
+    <TableResults
+      :title="title"
+      :columns="columns"
+      :model="model"
+      :routes="routes"
+    >
+      <template #fields="slotProps">
+        <span v-if="slotProps.props.column.field == '__str__'">
+          <MigasLink
+            model="singularities"
+            :pk="slotProps.props.row.id"
+            :value="slotProps.props.row.__str__"
+          />
+        </span>
+
+        <span v-else-if="slotProps.props.column.field == 'enabled'">
+          <BooleanView :value="slotProps.props.row.enabled" />
+        </span>
+
+        <span v-else-if="slotProps.props.column.field == 'property_att'">
+          <MigasLink
+            model="formulas"
+            :pk="slotProps.props.row.property_att.id"
+            :value="slotProps.props.row.property_att.name || ''"
+          />
+        </span>
+
+        <span v-else>
+          {{ slotProps.props.formattedRow[slotProps.props.column.field] }}
+        </span>
+      </template>
+    </TableResults>
+  </q-page>
+</template>
+
+<script>
+import { ref, reactive, onMounted } from 'vue'
+import { useGettext } from 'vue3-gettext'
+import { useMeta } from 'quasar'
+
+import { api } from 'boot/axios'
+import { useUiStore } from 'stores/ui'
+
+import Breadcrumbs from 'components/ui/Breadcrumbs'
+import TableResults from 'components/ui/TableResults'
+import BooleanView from 'components/ui/BooleanView'
+import MigasLink from 'components/MigasLink'
+
+import { appIcon, modelIcon } from 'composables/element'
+
+export default {
+  components: {
+    Breadcrumbs,
+    TableResults,
+    BooleanView,
+    MigasLink,
+  },
+  setup() {
+    const uiStore = useUiStore()
+    const { $gettext } = useGettext()
+
+    useMeta({ title: $gettext('Singularities List') })
+
+    const routes = {
+      add: 'singularity-add',
+      detail: 'singularity-detail',
+    }
+    const model = 'singularities'
+
+    const title = ref($gettext('Singularities'))
+
+    const breadcrumbs = reactive([
+      {
+        text: $gettext('Dashboard'),
+        icon: appIcon('home'),
+        to: 'home',
+      },
+      {
+        text: $gettext('Configuration'),
+        icon: appIcon('configuration'),
+      },
+      {
+        text: $gettext('Singularities'),
+        icon: modelIcon(model),
+      },
+      {
+        text: $gettext('Results'),
+        icon: appIcon('results'),
+      },
+    ])
+
+    const columns = reactive([
+      {
+        field: 'id',
+        hidden: true,
+      },
+      {
+        label: $gettext('Actions'),
+        field: 'actions',
+        html: true,
+        sortable: false,
+        globalSearchDisabled: true,
+      },
+      {
+        label: $gettext('Name'),
+        field: '__str__',
+        html: true,
+      },
+      {
+        label: $gettext('Enabled'),
+        field: 'enabled',
+        filterOptions: {
+          enabled: true,
+          placeholder: $gettext('All'),
+          trigger: 'enter',
+          filterDropdownItems: [
+            { value: true, text: $gettext('Yes') },
+            { value: false, text: $gettext('No') },
+          ],
+        },
+      },
+      {
+        label: $gettext('Formula'),
+        field: 'property_att',
+        filterOptions: {
+          enabled: true,
+          placeholder: $gettext('All'),
+          trigger: 'enter',
+        },
+      },
+      {
+        label: $gettext('Priority'),
+        field: 'priority',
+        type: 'number',
+        filterOptions: {
+          enabled: true,
+          trigger: 'enter',
+        },
+      },
+    ])
+
+    const loadFilters = async () => {
+      await api
+        .get('/api/v1/token/formulas/')
+        .then((response) => {
+          columns.find(
+            (x) => x.field === 'property_att'
+          ).filterOptions.filterDropdownItems = response.data.results.map(
+            (item) => {
+              return {
+                value: item.id,
+                text: item.name,
+              }
+            }
+          )
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+    }
+
+    onMounted(async () => {
+      await loadFilters()
+    })
+
+    return {
+      title,
+      breadcrumbs,
+      columns,
+      routes,
+      model,
+    }
+  },
+}
+</script>
