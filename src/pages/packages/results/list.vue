11c16f2a0cadce0d9d7c49cb969b74c75a01c22a
jact <jachavar@gmail.com>
2023-11-06 11:16:07 +0100

    refactor: prettier formatting

--- src/pages/packages/results/list.vue
+++ src/pages/packages/results/list.vue
@@ -157,14 +157,14 @@ export default {
         .get('/api/v1/token/projects/')
         .then((response) => {
           columns.find(
-            (x) => x.field === 'project.name'
+            (x) => x.field === 'project.name',
           ).filterOptions.filterDropdownItems = response.data.results.map(
             (item) => {
               return {
                 value: item.id,
                 text: item.name,
               }
-            }
+            },
           )
         })
         .catch((error) => {
@@ -175,14 +175,14 @@ export default {
         .get('/api/v1/token/stores/')
         .then((response) => {
           columns.find(
-            (x) => x.field === 'store.name'
+            (x) => x.field === 'store.name',
           ).filterOptions.filterDropdownItems = response.data.results.map(
             (item) => {
               return {
                 value: item.id,
                 text: `${item.name} (${item.project.name})`,
               }
-            }
+            },
           )
         })
         .catch((error) => {
