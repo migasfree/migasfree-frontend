2f4fa35cdddfdf26b2526a19b7b1f4006df84564
jact <jachavar@gmail.com>
2023-12-22 12:26:14 +0100

    feat: added titleIcon constant

--- src/pages/status-logs/index.vue
+++ src/pages/status-logs/index.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <Breadcrumbs :items="breadcrumbs" />
 
-    <Header :title="title" :has-export-button="false" />
+    <Header :title="title" :icon="titleIcon" :has-export-button="false" />
 
     <SearchFilter v-model="searchText" class="q-pb-md" @search="search" />
 
@@ -56,8 +56,9 @@ export default {
     const router = useRouter()
     const { $gettext } = useGettext()
 
-    const title = ref($gettext('Status Logs'))
-    useMeta({ title: title.value })
+    const titleIcon = modelIcon('status-logs')
+    const title = $gettext('Status Logs')
+    useMeta({ title })
 
     const searchText = ref('')
 
@@ -72,8 +73,8 @@ export default {
         icon: appIcon('data'),
       },
       {
-        text: title.value,
-        icon: modelIcon('status-logs'),
+        text: title,
+        icon: titleIcon,
       },
     ])
 
@@ -119,6 +120,7 @@ export default {
 
     return {
       title,
+      titleIcon,
       searchText,
       breadcrumbs,
       url,
