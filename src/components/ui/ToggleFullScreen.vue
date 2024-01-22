bda7c7776243e4aaa7e5b6a805757e02f686325f
jact <jachavar@gmail.com>
2024-01-22 09:37:09 +0100

    feat: added toggle full screen component

---
+++ src/components/ui/ToggleFullScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-btn
+    v-if="$q.screen.gt.sm"
+    round
+    flat
+    :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+    @click="$q.fullscreen.toggle()"
+  >
+    <q-tooltip>{{
+      $q.fullscreen.isActive
+        ? $gettext('Switch to Regular Screen view')
+        : $gettext('Switch to Full Screen view')
+    }}</q-tooltip>
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: 'ToggleFullScreen',
+}
+</script>
