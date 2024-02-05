c18dd235bd6fc43fe6c8608f66946591b8f47371
jact <jachavar@gmail.com>
2024-02-05 10:17:51 +0100

    refactor: usage of model

--- src/pages/domains/results/list.vue
+++ src/pages/domains/results/list.vue
@@ -12,7 +12,7 @@
       <template #fields="slotProps">
         <span v-if="slotProps.props.column.field == 'name'">
           <MigasLink
-            model="domains"
+            :model="model"
             :pk="slotProps.props.row.id"
             :value="slotProps.props.row.name"
           />
