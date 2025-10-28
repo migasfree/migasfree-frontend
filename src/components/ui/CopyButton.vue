b58abb9821682de8a2d421d7793fa14c01d915a3
jact <jachavar@gmail.com>
2025-10-28 20:51:14 +0100

    feat: added CopyButton component

---
+++ src/components/ui/CopyButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-btn
+    flat
+    icon="mdi-content-copy"
+    size="sm"
+    color="primary"
+    @click.stop="contentToClipboard(content)"
+    ><q-tooltip>{{ $gettext('Copy') }}</q-tooltip></q-btn
+  >
+</template>
+
+<script>
+import useCopyPaste from 'composables/copyPaste'
+
+export default {
+  name: 'CopyButton',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const { contentToClipboard } = useCopyPaste()
+
+    return {
+      contentToClipboard,
+    }
+  },
+}
+</script>
