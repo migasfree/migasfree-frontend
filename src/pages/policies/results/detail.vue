c5bf019228de6ff61746b74bf09106b96ac309d6
jact <jachavar@gmail.com>
2025-10-30 08:45:59 +0100

    refactor: appIcon usage

--- src/pages/policies/results/detail.vue
+++ src/pages/policies/results/detail.vue
@@ -115,7 +115,7 @@
                   dense
                   round
                   color="negative"
-                  icon="mdi-delete"
+                  :icon="appIcon('delete')"
                   @click="removeInline(index)"
                   ><q-tooltip>{{ $gettext('Delete') }}</q-tooltip></q-btn
                 >
