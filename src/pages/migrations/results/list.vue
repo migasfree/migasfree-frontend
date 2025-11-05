8807fe0e9f5d42b9c1fea2e7a87ed4639686cb73
Jose Antonio Chavarría <jachavar@gmail.com>
2025-11-05 12:56:13 +0100

    refactor: deconstruct slotProps, replaced reactive by ref

--- src/pages/migrations/results/list.vue
+++ src/pages/migrations/results/list.vue
@@ -8,31 +8,31 @@
       :model="model"
       :more-filters="moreFilters"
     >
-      <template #fields="slotProps">
-        <span v-if="slotProps.props.column.field == 'computer.__str__'">
+      <template #fields="{ props }">
+        <span v-if="props.column.field == 'computer.__str__'">
           <MigasLink
             model="computers"
-            :pk="slotProps.props.row.computer.id"
-            :value="slotProps.props.row.computer.__str__"
-            :icon="elementIcon(slotProps.props.row.computer.status)"
-            :tooltip="slotProps.props.row.computer.summary"
+            :pk="props.row.computer.id"
+            :value="props.row.computer.__str__"
+            :icon="elementIcon(props.row.computer.status)"
+            :tooltip="props.row.computer.summary"
           />
         </span>
 
-        <span v-else-if="slotProps.props.column.field == 'project.name'">
+        <span v-else-if="props.column.field == 'project.name'">
           <MigasLink
             model="projects"
-            :pk="slotProps.props.row.project.id"
-            :value="slotProps.props.row.project.name || ''"
+            :pk="props.row.project.id"
+            :value="props.row.project.name || ''"
           />
         </span>
 
-        <span v-else-if="slotProps.props.column.field == 'created_at'">
-          <DateView :value="slotProps.props.row.created_at" />
+        <span v-else-if="props.column.field == 'created_at'">
+          <DateView :value="props.row.created_at" />
         </span>
 
         <span v-else>
-          {{ slotProps.props.formattedRow[slotProps.props.column.field] }}
+          {{ props.formattedRow[props.column.field] }}
         </span>
       </template>
     </TableResults>
@@ -40,7 +40,7 @@
 </template>
 
 <script>
-import { ref, reactive, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
@@ -73,7 +73,7 @@ export default {
 
     const title = ref($gettext('Migrations'))
 
-    const breadcrumbs = reactive([
+    const breadcrumbs = ref([
       {
         text: $gettext('Dashboard'),
         icon: appIcon('home'),
@@ -94,7 +94,7 @@ export default {
       },
     ])
 
-    const columns = reactive([
+    const columns = ref([
       {
         field: 'id',
         hidden: true,
@@ -150,15 +150,13 @@ export default {
       await api
         .get('/api/v1/token/projects/')
         .then((response) => {
-          columns.find(
+          columns.value.find(
             (x) => x.field === 'project.name',
           ).filterOptions.filterDropdownItems = response.data.results.map(
-            (item) => {
-              return {
-                value: item.id,
-                text: item.name,
-              }
-            },
+            ({ id, name }) => ({
+              value: id,
+              text: name,
+            }),
           )
         })
         .catch((error) => {
