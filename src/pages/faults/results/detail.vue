9644eb1b59b6fdaf0e61cae0b57b9ff816b026e4
Jose Antonio Chavarría <jachavar@gmail.com>
2025-11-05 12:33:13 +0100

    refactor: replaced reactive by ref

--- src/pages/faults/results/detail.vue
+++ src/pages/faults/results/detail.vue
@@ -126,7 +126,7 @@ export default {
 
     let element = reactive({ id: 0 })
 
-    const breadcrumbs = reactive([
+    const breadcrumbs = ref([
       {
         text: $gettext('Dashboard'),
         icon: appIcon('home'),
