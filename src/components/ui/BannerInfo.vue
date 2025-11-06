4a489ad1cf98178c376af89d6a6c97cef612eb92
jact <jachavar@gmail.com>
2025-11-06 18:14:06 +0100

    refactor: appIcon usage

--- src/components/ui/BannerInfo.vue
+++ src/components/ui/BannerInfo.vue
@@ -1,17 +1,22 @@
 <template>
   <q-banner rounded class="bg-light-blue-2 text-black q-ma-md">
     <template #avatar>
-      <q-icon name="mdi-information-outline" color="black" />
+      <q-icon :name="appIcon('information')" color="black" />
     </template>
     {{ message }}
   </q-banner>
 </template>
 
 <script>
+import { appIcon } from 'composables/element'
+
 export default {
   name: 'BannerInfo',
   props: {
     message: { type: String, required: true },
+  },
+  setup() {
+    return { appIcon }
   },
 }
 </script>
