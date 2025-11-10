7477be95842d94194e02508d8366f0a0b4f8915e
jact <jachavar@gmail.com>
2025-11-10 12:38:58 +0100

    feat: added ScheduleProgress component

---
+++ src/components/deployment/ScheduleProgress.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-linear-progress
+    :size="size"
+    :value="timeline.percent / 100"
+    color="secondary"
+  >
+    <div class="absolute-full flex flex-center">
+      <q-badge
+        color="white"
+        text-color="primary"
+        :label="`${timeline.percent}%`"
+      />
+    </div>
+
+    <q-tooltip>
+      <q-list dense>
+        <q-item>{{ $gettext('Start') }}: {{ timeline.begin_date }}</q-item>
+        <q-item>{{ $gettext('End') }}: {{ timeline.end_date }}</q-item>
+      </q-list>
+    </q-tooltip>
+  </q-linear-progress>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleProgress',
+  props: {
+    timeline: {
+      type: Object,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: '24px',
+    },
+  },
+}
+</script>
