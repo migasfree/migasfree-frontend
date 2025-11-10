6a9e9a480e59fb2696ba0f1bc7460e996d561d7a
jact <jachavar@gmail.com>
2025-11-10 14:20:32 +0100

    refactor: useMeta

--- src/pages/manufacturers/results/detail.vue
+++ src/pages/manufacturers/results/detail.vue
@@ -50,11 +50,7 @@ export default {
 
     const title = ref($gettext('Manufacturer'))
     const windowTitle = ref(title.value)
-    useMeta(() => {
-      return {
-        title: windowTitle.value,
-      }
-    })
+    useMeta(() => ({ title: windowTitle.value }))
 
     const routes = {
       list: 'manufacturers-list',
