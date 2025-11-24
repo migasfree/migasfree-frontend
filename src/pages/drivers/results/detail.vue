1c8b7038464b9fca32ae2b7be8ecf89d0c42a578
jact <jachavar@gmail.com>
2025-11-24 10:33:35 +0100

    feat: added related links

--- src/pages/drivers/results/detail.vue
+++ src/pages/drivers/results/detail.vue
@@ -89,6 +89,15 @@
                   />
                 </template>
               </q-select>
+
+              <q-btn
+                color="secondary"
+                class="q-ma-sm"
+                :icon="appIcon('add')"
+                :icon-right="modelIcon('projects')"
+                @click="$router.push({ name: 'project-add' })"
+                ><q-tooltip>{{ $gettext('Add Project') }}</q-tooltip></q-btn
+              >
             </div>
 
             <div class="col-6 col-md col-sm">
@@ -119,6 +128,15 @@
                   />
                 </template>
               </q-select>
+
+              <q-btn
+                color="secondary"
+                class="q-ma-sm"
+                :icon="appIcon('add')"
+                :icon-right="modelIcon('devices/capabilities')"
+                @click="$router.push({ name: 'capability-add' })"
+                ><q-tooltip>{{ $gettext('Add Capability') }}</q-tooltip></q-btn
+              >
             </div>
           </div>
 
@@ -138,6 +156,7 @@
 
 <script>
 import { ref, reactive, computed } from 'vue'
+import { useRoute } from 'vue-router'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
@@ -160,6 +179,7 @@ export default {
   },
   setup() {
     const { $gettext } = useGettext()
+    const route = useRoute()
     const uiStore = useUiStore()
 
     const title = ref($gettext('Driver'))
@@ -173,7 +193,7 @@ export default {
     }
     const model = 'devices/drivers'
 
-    let element = reactive({
+    const element = reactive({
       id: 0,
       name: undefined,
       model: null,
@@ -224,6 +244,18 @@ export default {
         if (Array.isArray(element?.packages_to_install)) {
           element.packages_to_install = element.packages_to_install.join('\n')
         }
+
+        if (route.query.project)
+          element.project =
+            projects.value.find(
+              (item) => item.id === Number(route.query.project),
+            ) || null
+
+        if (route.query.capability)
+          element.capability =
+            capabilities.value.find(
+              (item) => item.id === Number(route.query.capability),
+            ) || null
       } catch (error) {
         uiStore.notifyError(error)
       }
@@ -278,6 +310,7 @@ export default {
       elementData,
       resetElement,
       setTitle,
+      appIcon,
       modelIcon,
       filterModels,
     }
