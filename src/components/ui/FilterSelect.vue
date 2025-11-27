3367061ed83bbf633ae6c14af43531a4f6422dd5
Jose Antonio Chavarría <jachavar@gmail.com>
2025-11-27 09:37:01 +0100

    feat: added FilterSelect component (to avoid duplicated code)

---
+++ src/components/ui/FilterSelect.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-select
+    :model-value="modelValue"
+    :options="options"
+    :label="label"
+    dense
+    outlined
+    option-value="id"
+    option-label="name"
+    @update:model-value="$emit('update:modelValue', $event)"
+  >
+    <template #before>
+      <q-icon :name="appIcon('filter')" />
+    </template>
+  </q-select>
+</template>
+
+<script>
+import { appIcon } from 'composables/element'
+
+export default {
+  name: 'FilterSelect',
+
+  props: {
+    modelValue: {
+      type: [Object, String, Number],
+      default: null,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {
+      appIcon,
+    }
+  },
+
+  emits: ['update:modelValue'],
+}
+</script>
