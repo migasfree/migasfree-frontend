a302462e35139256f17e8155876ea6b139b72e6f
jact <jachavar@gmail.com>
2025-11-30 14:41:49 +0100

    fix: improved automated tests (data-test attribute)

--- src/components/ui/AppMenu.vue
+++ src/components/ui/AppMenu.vue
@@ -9,7 +9,13 @@
     >
       <q-list v-for="option in item.options" :key="option.to">
         <q-separator v-if="option.separatorBefore" />
-        <q-item clickable :to="{ name: option.to }" :inset-level="0.5" exact>
+        <q-item
+          clickable
+          :to="{ name: option.to }"
+          :inset-level="0.5"
+          exact
+          :data-test="`menu-item-${option.to}`"
+        >
           <q-item-section v-if="option.icon" avatar>
             <q-icon :name="option.icon" size="sm" />
           </q-item-section>
