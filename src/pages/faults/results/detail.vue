df8067e23792040118deeda0e1c94c0d513e7f6b
jact <jachavar@gmail.com>
2025-12-19 14:20:06 +0100

    fix: avoid a11y errors

--- src/pages/faults/results/detail.vue
+++ src/pages/faults/results/detail.vue
@@ -18,9 +18,9 @@
           <div class="row q-pa-md q-gutter-md">
             <div class="col-6 col-md col-sm">
               <q-checkbox
+                ref="primaryInput"
                 v-model="element.checked"
                 left-label
-                autofocus
                 :label="$gettext('Checked?')"
               />
             </div>
@@ -108,6 +108,7 @@ import ItemDetail from 'components/ui/ItemDetail'
 import MigasLink from 'components/MigasLink'
 
 import { appIcon, modelIcon, useElement } from 'composables/element'
+import useAutoFocus from 'composables/useAutoFocus'
 
 export default {
   components: {
@@ -119,6 +120,7 @@ export default {
   setup() {
     const { $gettext } = useGettext()
     const { elementIcon } = useElement()
+    const { inputRef: primaryInput } = useAutoFocus()
 
     const title = ref($gettext('Fault'))
     const windowTitle = ref(title.value)
@@ -172,6 +174,7 @@ export default {
       elementData,
       setTitle,
       modelIcon,
+      primaryInput,
     }
   },
 }
