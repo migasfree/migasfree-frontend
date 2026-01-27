a29a9ac33841cddd36739f43cf9a22206c799a48
Jose Antonio Chavarría <jachavar@gmail.com>
2026-01-27 11:30:20 +0100

    refactor(Manufacturers): remove redundant props in favor of global defaults

--- src/pages/manufacturers/results/detail.vue
+++ src/pages/manufacturers/results/detail.vue
@@ -35,7 +35,6 @@
               <q-input
                 ref="primaryInput"
                 v-model="element.name"
-                outlined
                 :label="$gettext('Name')"
                 lazy-rules
                 :rules="[(val) => !!val || $gettext('* Required')]"
