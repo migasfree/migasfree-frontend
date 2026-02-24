e2472219b73ce1658cc2fb75b33d835fb5f93dd6
Jose Antonio Chavarría <jachavar@gmail.com>
2026-02-24 11:30:16 +0100

    fix(dateview): wrap template in a single root element
    Enclose the multiple root elements of DateView within a single `<span>` to avoid Vue 3 warnings about extraneous non-props attributes when the component renders fragments.

--- src/components/ui/DateView.vue
+++ src/components/ui/DateView.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-icon v-if="icon" :name="icon" size="sm" class="vertical-middle" />
-  <span v-if="value" class="vertical-middle">
-    {{ showDate(value) }}
-    <q-tooltip>
-      <template v-if="tooltipText">
-        {{ tooltipText }} ({{ diffForHumans(value) }})
-      </template>
-      <template v-else>{{ diffForHumans(value) }} </template></q-tooltip
-    >
+  <span>
+    <q-icon v-if="icon" :name="icon" size="sm" class="vertical-middle" />
+    <span v-if="value" class="vertical-middle">
+      {{ showDate(value) }}
+      <q-tooltip>
+        <template v-if="tooltipText">
+          {{ tooltipText }} ({{ diffForHumans(value) }})
+        </template>
+        <template v-else>{{ diffForHumans(value) }} </template>
+      </q-tooltip>
+    </span>
   </span>
 </template>
 
